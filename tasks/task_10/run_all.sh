8bd94aae245b4c55b9bb71efd51d7c19346b96e4
shimwell <mail@jshimwell.com>
2019-11-21 16:23:02 +0000

    added outputfilenames

--- tasks/task_10/run_all.sh
+++ tasks/task_10/run_all.sh
@@ -1,21 +1,11 @@
-
 trelis -batch -nographics make_faceted_geometry_with_materials.py 
 
-make_watertight dagmc_notwatertight.h5m 
+make_watertight dagmc_notwatertight.h5m -o dagmc_watertight.h5m
 
-mv dagmc_notwatertight_zip.h5m dagmc_watertight.h5m
-
-check_watertight dagmc_watertight.h5m
-
-mv dagmc_watertight.h5m dagmc_without_obb.h5m
-
-build_obb dagmc_without_obb.h5m
-
-mv dagmc_without_obb_obb.h5m dagmc.h5m
+build_obb dagmc_watertight.h5m -o dagmc.h5m
 
 mbconvert dagmc.h5m dagmc.stl
 
 mbconvert dagmc.h5m dagmc.vtk
 
 python3 example_CAD_simulations.py
-
