99f353a234a2acbb5aa4c8072aaf9190e68520d5
shimwell <mail@jshimwell.com>
2019-11-22 16:57:09 +0000

    renamed filepaths

--- multistage_docker_image/docker_run_tendl.sh
+++ multistage_docker_image/docker_run_tendl.sh
@@ -1,3 +1,3 @@
-sudo docker build -f Dockerfile_openmc -t jbillingsley/multistage:latest .
-sudo docker build -f Dockerfile_nuclear_data_tendl -t jbillingsley/multistage:latest .
-sudo docker build -f Dockerfile_tasks -t jbillingsley/multistage:latest .
+sudo docker build -f Dockerfile_openmc -t openmcworkshop/openmc:latest .
+sudo docker build -f Dockerfile_nuclear_data_tendl -t openmcworkshop/nuclear_data_tendl:latest .
+sudo docker build -f Dockerfile_tasks -t openmcworkshop/tasks:latest .
