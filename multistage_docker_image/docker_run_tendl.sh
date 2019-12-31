94503745c2e8c81f54eb2d5fe3f1356fb52aceac
shimwell <mail@jshimwell.com>
2019-12-31 09:33:06 +0000

    new layout

--- multistage_docker_image/docker_run_tendl.sh
+++
@@ -1,3 +0,0 @@
-sudo docker build -f Dockerfile_openmc -t openmcworkshop/openmc_workshop_image_repository:latest .
-sudo docker build -f Dockerfile_workshop -t openmcworkshop/openmc_workshop_image_repository:latest .
-sudo docker build -f Dockerfile_nuclear_data_tendl -t openmcworkshop/openmc_workshop_image_repository:tendl .
