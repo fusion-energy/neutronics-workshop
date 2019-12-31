0c543024a426fa6868613b4596c42b9119f1cdb0
shimwell <mail@jshimwell.com>
2019-12-31 09:33:06 +0000

    new layout

--- multistage_docker_image/run_fendl.sh
+++
@@ -1,2 +0,0 @@
-xhost local:root
-sudo docker run --net=host -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/openmc_workshop -e DISPLAY=unix$DISPLAY --privileged openmcworkshop/openmc_workshop_image_repository:fendl
