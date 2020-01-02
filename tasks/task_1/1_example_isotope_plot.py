a6efd0d266b0fd0e1d2ea6aed85ab9831fb90e58
shimwell <shimwell@yahoo.com>
2020-01-02 15:59:55 +0000

    moved to new plotly stlye using fig

--- tasks/task_1/1_example_isotope_plot.py
+++ tasks/task_1/1_example_isotope_plot.py
@@ -44,11 +44,46 @@ fig.update_layout(
       title = 'Isotope cross sections MT '+ str(MT_number),
       xaxis = {'title':'Energy (eV)',
                'range':(0,14.1e6)
-               #'type':'log'
                },
       yaxis = {'title':'Cross section (barns)'
-               #'type':'log'
               }
+)
+
+
+# this adds the dropdown box for log and lin axis selection
+fig.update_layout(
+    updatemenus=[
+        go.layout.Updatemenu(
+            buttons=list([
+                dict(
+                    args=[{"xaxis.type":'lin', "yaxis.type":'lin', 'yaxis.range':(0,14.1e6)}],
+                    label="linear(x) , linear(y)",
+                    method="relayout"
+                ),
+                dict(
+                    args=[{"xaxis.type":'log', "yaxis.type":'log'}],
+                    label="log(x) , log(y)",
+                    method="relayout"
+                ),
+                dict(
+                    args=[{"xaxis.type":'log', "yaxis.type":'lin', 'yaxis.range':(0,14.1e6)}],
+                    label="log(x) , linear(y)",
+                    method="relayout"
+                ),
+                dict(
+                    args=[{"xaxis.type":'lin', "yaxis.type":'log'}],
+                    label="linear(x) , log(y)",
+                    method="relayout"
+                )
+            ]),
+            pad={"r": 10, "t": 10},
+            showactive=True,
+            x=0.5,
+            xanchor="left",
+            y=1.1,
+            yanchor="top"
+        ),
+    ]
 )
 
 fig.write_html("1_example_isotope_plot.html")
