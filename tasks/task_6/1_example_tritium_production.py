739ac4497d19ef30538038baaff9cd91e9694cf2
shimwell <shimwell@yahoo.com>
2020-01-06 18:13:16 +0000

    converted to new plotly added test script beta

---
+++ tasks/task_6/1_example_tritium_production.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+"""example_tritium_production.py: plots few 2D views of a simple tokamak geometry with neutron flux."""
+
+__author__      = "Jonathan Shimwell"
+
+import openmc
+
+
+#MATERIALS#
+
+breeder_material = openmc.Material(1, "PbLi") #Pb84.2Li15.8 with natural enrichment of Li6
+enrichment_fraction = 0.50
+breeder_material.add_element('Pb', 84.2,'ao')
+breeder_material.add_nuclide('Li6', enrichment_fraction*15.8, 'ao')
+breeder_material.add_nuclide('Li7', (1.0-enrichment_fraction)*15.8, 'ao')
+breeder_material.set_density('atom/b-cm',3.2720171e-2) # around 11 g/cm3
+
+copper = openmc.Material(name='Copper')
+copper.set_density('g/cm3', 8.5)
+copper.add_element('Cu', 1.0)
+
+eurofer = openmc.Material(name='EUROFER97')
+eurofer.set_density('g/cm3', 7.75)
+eurofer.add_element('Fe', 89.067, percent_type='wo')
+eurofer.add_element('C', 0.11, percent_type='wo')
+eurofer.add_element('Mn', 0.4, percent_type='wo')
+eurofer.add_element('Cr', 9.0, percent_type='wo')
+eurofer.add_element('Ta', 0.12, percent_type='wo')
+eurofer.add_element('W', 1.1, percent_type='wo')
+eurofer.add_element('N', 0.003, percent_type='wo')
+eurofer.add_element('V', 0.2, percent_type='wo')
+
+mats = openmc.Materials([breeder_material, eurofer, copper])
+
+
+#GEOMETRY#
+
+#surfaces
+central_sol_surface = openmc.ZCylinder(r=100)
+central_shield_outer_surface = openmc.ZCylinder(r=110)
+vessel_inner = openmc.Sphere(r=500)
+first_wall_outer_surface = openmc.Sphere(r=510)
+breeder_blanket_outer_surface = openmc.Sphere(r=610,boundary_type='vacuum')
+
+
+#cells
+central_sol_region = -central_sol_surface & -breeder_blanket_outer_surface
+central_sol_cell = openmc.Cell(region=central_sol_region) 
+central_sol_cell.fill = copper
+
+central_shield_region = +central_sol_surface & -central_shield_outer_surface & -breeder_blanket_outer_surface
+central_shield_cell = openmc.Cell(region=central_shield_region) 
+central_shield_cell.fill = eurofer
+
+inner_vessel_region = -vessel_inner & + central_shield_outer_surface
+inner_vessel_cell = openmc.Cell(region=inner_vessel_region) 
+
+first_wall_region = -first_wall_outer_surface & +vessel_inner
+first_wall_cell = openmc.Cell(region=first_wall_region) 
+first_wall_cell.fill = eurofer
+
+breeder_blanket_region = +first_wall_outer_surface & -breeder_blanket_outer_surface & +central_shield_outer_surface
+breeder_blanket_cell = openmc.Cell(region=breeder_blanket_region) 
+breeder_blanket_cell.fill = breeder_material
+
+universe = openmc.Universe(cells=[central_sol_cell,central_shield_cell,inner_vessel_cell,first_wall_cell, breeder_blanket_cell])
+geom = openmc.Geometry(universe)
+
+
+
+
+#SIMULATION SETTINGS#
+
+# Instantiate a Settings object
+sett = openmc.Settings()
+batches = 2
+sett.batches = batches
+sett.inactive = 0
+sett.particles = 50
+sett.run_mode = 'fixed source'
+
+# Create a DT point source
+source = openmc.Source()
+source.space = openmc.stats.Point((300,0,0))
+source.angle = openmc.stats.Isotropic()
+source.energy = openmc.stats.Discrete([14e6], [1])
+sett.source = source
+
+
+tallies = openmc.Tallies()
+
+#added a cell tally for tritium production
+cell_filter = openmc.CellFilter(breeder_blanket_cell)
+tbr_tally = openmc.Tally(name='TBR')
+tbr_tally.filters = [cell_filter]
+tbr_tally.scores = ['(n,Xt)'] # MT 205 is the (n,Xt) reaction where X is a wildcard, if MT 105 or (n,t) then some tritium production will be missed, for example (n,nt) which happens in Li7 would be missed
+tallies.append(tbr_tally)
+
+
+# Run OpenMC!
+model = openmc.model.Model(geom, mats, sett, tallies)
+model.run()
+
+# open the results file
+sp = openmc.StatePoint('statepoint.'+str(batches)+'.h5')
+
+# access the tally using pandas dataframes
+tbr_tally = sp.get_tally(name='TBR')
+df = tbr_tally.get_pandas_dataframe()
+
+tbr_tally_result = df['mean'].sum()
+tbr_tally_std_dev = df['std. dev.'].sum()
+
+
+print('The tritium breeding ratio was found, TBR = ',tbr_tally_result)
+print('error on the tbr tally is ',tbr_tally_std_dev)
