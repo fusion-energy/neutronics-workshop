862ed356cc351e713bb24e0da85bda2e3549acf6
shimwell <shimwell@yahoo.com>
2020-01-06 17:08:41 +0000

    moved to the new plotly format

--- tasks/task_5/1_example_neutron_spectra_tokamak.py
+++ tasks/task_5/1_example_neutron_spectra_tokamak.py
@@ -128,7 +128,7 @@ fig.add_trace(go.Scatter(x=energy_bins,
                      )
               )
 
-# this will need to be uncommented to add the spectra to the plot
+# this will need to be uncommented to add the first wall spectra to the plot
 # spectra_tally = sp.get_tally(name='first_wall_spectra') # add another tally for first_wall_spectra
 # df = spectra_tally.get_pandas_dataframe()
 # spectra_tally_result = df['mean']
