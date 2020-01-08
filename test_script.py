632fa8c830a4f5fc42ce8301c983ede1f1e572d3
billingsley-john <johnbillingsley1996@yahoo.com>
2020-01-08 11:50:02 +0000

    added more tests

--- test_script.py
+++ test_script.py
@@ -1,5 +1,5 @@
 
-""" test_script.py: run all the examples in the openmc workshop and check each example produces the exspected results.
+""" test_script.py: run all the examples in the openmc workshop and check each example produces the expected results.
     The example scripts open up various plots, some of which pause / block the running of this test suite.
     The running of the tests can be manually resumed by closing matplotlib and eog windows when they pop up
     run with
@@ -11,17 +11,20 @@ import os
 import pytest
 import unittest
 
+cwd = os.getcwd()
+
 class test_task_1(unittest.TestCase):
     def test_task_1_part_1(self):
 
-        cwd = os.getcwd()
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
         os.chdir(Path('tasks/task_1'))
-        output_filename = '1_example_isotope_plot.html'
+        output_filename = '1_example_error_isotope_plot.html'
         os.system('rm '+output_filename)
         os.system('python 1_example_isotope_plot.py')
         assert Path(output_filename).exists() == True
         os.system('rm '+output_filename)
-        os.chdir(Path(cwd))
+        # os.chdir(Path(cwd))
 
 
     # def test_task_1_part_2(self): #  this test is slow so it has been commented out for the time being
@@ -38,96 +41,184 @@ class test_task_1(unittest.TestCase):
 
     def test_task_1_part_3(self):
         
-        cwd = os.getcwd()
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
         os.chdir(Path('tasks/task_1'))
         output_filename = '3_example_material_plot.html'
         os.system('rm '+output_filename)
         os.system('python 3_example_material_plot.py')
         assert Path(output_filename).exists() == True
         os.system('rm '+output_filename)
-        os.chdir(Path(cwd))
+        # os.chdir(Path(cwd))
 
 
 class test_task_2(unittest.TestCase):
     
     def test_task_2_part_1(self): #  This test launches matplotlib that pauses the running of the script
         
-        cwd = os.getcwd()
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
         os.chdir(Path('tasks/task_2'))
         output_filenames = ['xz_sphere.png', 'xy_sphere.png', 'yz_sphere.png']
+        for output_filename in output_filenames:
+            os.system('rm '+output_filename)
         os.system('python 1_example_geometry_viewer_2d.py')
         for output_filename in output_filenames:
             assert Path(output_filename).exists() == True
             os.system('rm '+output_filename)
-        os.chdir(Path(cwd))
+        # os.chdir(Path(cwd))
 
 
     def test_task_2_part_1_optional(self): #  This test launches eog that pauses the running of the script
 
-        cwd = os.getcwd()
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
         os.chdir(Path('tasks/task_2'))
         output_filename = 'plot.png'
+        os.system('rm '+output_filename)
         os.system('python 2_example_geometry_viewer_2d_xml_version.py')
         assert Path(output_filename).exists() == True
         os.system('rm '+output_filename)
-        os.chdir(Path(cwd))
+        # os.chdir(Path(cwd))
 
 
     def test_task_2_part_3(self): #  This test launches matplotlib that pauses the running of the script
-        cwd = os.getcwd()
+
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
         os.chdir(Path('tasks/task_2'))
         output_filenames = ['xz_tokamak.png', 'xy_tokamak.png', 'yz_tokamak.png']
+        for output_filename in output_filenames:
+            os.system('rm '+output_filename)
         os.system('python 3_example_geometry_viewer_2d_tokamak.py')
         for output_filename in output_filenames:
             assert Path(output_filename).exists() == True
             os.system('rm '+output_filename)
-        os.chdir(Path(cwd))
+        # os.chdir(Path(cwd))
 
 
     def test_task_2_part_4(self):
+
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
+        os.chdir(Path('tasks/task_2'))
+        output_filename = 'plot_3d_tokamak.vti'
+        os.system('rm '+output_filename)
+        os.system('python 4_example_geometry_viewer_3d_tokamak.py')
+        assert Path(output_filename).exists() == True
+        os.system('rm '+output_filename)
+        # os.chdir(Path(cwd))
+
         # python 4_example_geometry_viewer_3d_tokamak.py
         # paraview plot_3d_tokamak.vti
-        pass
 
 
 
 class test_task_3(unittest.TestCase):
     def test_task_3_part_1(self):
+
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
+        os.chdir(Path('tasks/task_3'))
+        output_filename = 'particle_energy_histogram.html'
+        os.system('rm '+output_filename)
+        os.system('python 1_plot_neutron_birth_energy.py')
+        assert Path(output_filename).exists() == True
+        os.system('rm '+output_filename)
+        # os.chdir(Path(cwd))
+
         # cd tasks/task_3
 
         # python 1_plot_neutron_birth_energy.py
         # particle_energy_histogram.html
-        pass
+    
     
     def test_task_3_part_2(self):
 
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
+        os.chdir(Path('tasks/task_3'))
+        output_filename = '3d_plot_cones.html'   # may change the name of this output
+        os.system('rm '+output_filename)
+        os.system('python 2_plot_neutron_birth_direction.py')
+        assert Path(output_filename).exists() == True
+        os.system('rm '+output_filename)
+        # os.chdir(Path(cwd))
+        
+
         # python 2_plot_neutron_birth_direction.py
-        # particle_direction.html
-        pass
+        # particle_direction.html (need to check this)
+        
 
     def test_task_3_part_3(self):
+
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
+        os.chdir(Path('tasks/task_3'))
+        output_filename = 'particle_location.html'
+        os.system('rm '+output_filename)
+        os.system('python 3_plot_neutron_birth_location.py')
+        assert Path(output_filename).exists() == True
+        os.system('rm '+output_filename)
+        # os.chdir(Path(cwd))
+        
+
         # python 3_plot_neutron_birth_location.py
         # particle_location.html
-        pass
 
     def test_task_3_part_4(self):
+
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
+        os.chdir(Path('tasks/task_3'))
+        output_filename = 'plasma_particle_direction.html'
+        os.system('rm '+output_filename)
+        os.system('python 4_plot_neutron_birth_direction_plasma.py')
+        assert Path(output_filename).exists() == True
+        os.system('rm '+output_filename)
+        # os.chdir(Path(cwd))
+        
+
         # python 4_plot_neutron_birth_direction_plasma
-        # plasma_particle_location.html
-        pass
+        # plasma_particle_direction.html
 
     def test_task_3_part_5(self):
+
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
+        os.chdir(Path('tasks/task_3'))
+        output_filename = 'plasma_particle_location.html'
+        os.system('rm '+output_filename)
+        os.system('python 5_plot_neutron_birth_location_plasma.py')
+        assert Path(output_filename).exists() == True
+        os.system('rm '+output_filename)
+        # os.chdir(Path(cwd))
+        
+
         # python 5_plot_neutron_birth_location_plasma.py
-        # plasma_particle_direction.html
-        pass
+        # plasma_particle_location.html
 
     def test_task_3_part_6(self):
+
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
+        os.chdir(Path('tasks/task_3'))
+        output_filenames = ['plot_3d.h5', 'plot_3d.vti', 'track_1_1_4.h5', 'track_1_1_4.pvtp', 'track_1_1_4_0.vtp']
+        for output_filename in output_filenames:
+            os.system('rm '+output_filename)
+        os.system('python 6_example_neutron_tracks.py')
+        for output_filename in output_filenames:
+            assert Path(output_filename).exists() == True
+            os.system('rm '+output_filename)
+        # os.chdir(Path(cwd))
+        
+
         # 6_example_neutron_tracks.py
         # plot_3d.h5
         # plot_3d.vti
         # track_1_1_4.h5
         # track_1_1_4.pvtp
         # track_1_1_4_0.vtp
-        pass
 
 
 class test_task_4(unittest.TestCase):
@@ -136,12 +227,25 @@ class test_task_4(unittest.TestCase):
 
         # python 1_example_neutron_flux
         # tally_on_mesh
+
+        # need to create an output file
         pass
 
     def test_task_4_part_2(self):
+
+        # cwd = os.getcwd()
+        os.chdir(Path(cwd))
+        os.chdir(Path('tasks/task_4'))
+        output_filename = 'tally_on_mesh.vtk'
+        os.system('rm '+output_filename)
+        os.system('python 2_example_neutron_flux_tokamak.py')
+        assert Path(output_filename).exists() == True
+        os.system('rm '+output_filename)
+        # os.chdir(Path(cwd))
+
         # python 2_example_neutron_flux_tokamak
         # tally_on_mesh.vtk
-        pass
+        
 
 
 
@@ -149,19 +253,48 @@ class test_task_5(unittest.TestCase):
     def test_task_5_part_1(self):
         # cd tasks/task_5
 
+        # cwd = os.getcwd()
+        os.chdir(Path('tasks/task_5'))
+        output_filename = 'tokamak_spectra.html'
+        os.system('rm '+output_filename)
+        os.system('python 1_example_neutron_spectra_tokamak.py')
+        assert Path(output_filename).exists() == True
+        os.system('rm '+output_filename)
+        os.chdir(Path(cwd))
+        
+
         # python 1_example_neutron_spectra_tokamak.py 
         # tokamak_spectra.html
-        pass
 
     def test_task_5_part_2(self):
+
+        # cwd = os.getcwd()
+        os.chdir(Path('tasks/task_5'))
+        output_filename = 'tokamak_photon_spectra.html'
+        os.system('rm '+output_filename)
+        os.system('python 2_example_photon_spectra_tokamak.py')
+        assert Path(output_filename).exists() == True
+        os.system('rm '+output_filename)
+        os.chdir(Path(cwd))
+        
+
         # python 2_example_photon_spectra_tokamak.py
         # tokamak_photon_spectra.html
-        pass
 
 
 class test_task_6(unittest.TestCase):
     def test_task_6_part_1(self):
+
+        # cwd = os.getcwd()
+        os.chdir(Path('tasks/task_6'))
+        output_filename = 'tbr_study.html'
+        os.system('rm '+output_filename)
+        os.system('python 2_example_tritium_production_study.py')
+        assert Path(output_filename).exists() == True
+        os.system('rm '+output_filename)
+        os.chdir(Path(cwd))
+        
+
         # cd tasks/task_6
         # python 2_example_tritium_production_study.py
         # tbr_study.html
-        pass
