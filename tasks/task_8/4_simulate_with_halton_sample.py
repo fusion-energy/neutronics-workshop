a1c6878f986fc0980a17703f33152b8abd53a94a
shimwell <shimwell@yahoo.com>
2020-01-27 08:50:49 +0000

    checking for exisiting simulations

--- tasks/task_8/4_simulate_with_halton_sample.py
+++ tasks/task_8/4_simulate_with_halton_sample.py
@@ -147,8 +147,11 @@ path_to_json = "outputs"
 list_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
 resultdict = []
 for filename in list_files:
-    with open(os.path.join(path_to_json, filename), "r") as inputjson:
-        resultdict.append(json.load(inputjson))
+    try:
+        with open(os.path.join(path_to_json, filename), "r") as inputjson:
+            resultdict.append(json.load(inputjson))
+    except:
+        print('no exisiting simulations to add to')
 results_df = pd.DataFrame(resultdict)
 
 number_of_new_simulations = 10 # this value will need to be changed
@@ -160,16 +163,16 @@ for i in tqdm(range(number_of_new_simulations)):
         existing_simulations_for_this_material = results_df[results_df['breeder_material_name']=='Li']
 
         sequencer = ghalton.Halton(2)
-        x = sequencer.get(number_of_simulations+len(existing_simulations_for_this_material))
-        x = [item for sublist in x for item in sublist]
-
-        enrichment_fraction = x[0]
-        thickness = x[1]*500
-        result = make_geometry_tallies(batches=2, # this value can be increased to decrease error
-                                    nps=1000,  
-                                    enrichment_fraction=enrichment_fraction,
-                                    inner_radius=500,
-                                    thickness=thickness,
-                                    breeder_material_name=breeder_material_name,
-                                    temperature_in_C=500
-                                    )
+        coordinates = sequencer.get(number_of_simulations+len(existing_simulations_for_this_material))
+        coordinates = [item for sublist in x for item in sublist]
+        for coord in coordinates:
+            enrichment_fraction = coord[0]
+            thickness = coord*500
+            result = make_geometry_tallies(batches=2, # this value can be increased to decrease error
+                                        nps=1000,  
+                                        enrichment_fraction=enrichment_fraction,
+                                        inner_radius=500,
+                                        thickness=thickness,
+                                        breeder_material_name=breeder_material_name,
+                                        temperature_in_C=500
+                                        )
