01bb2d3c2476770239f5142dd9809fb2667c5d28
shimwell <mail@jshimwell.com>
2020-02-01 00:11:23 +0000

    example working

--- tasks/task_10/run_all.sh
+++ tasks/task_10/run_all.sh
@@ -1,11 +1,9 @@
-trelis -batch -nographics make_faceted_geometry_with_materials.py 
+steps2h5m geometry_details.json 1. dagmc_notwatertight.h5m
 
-make_watertight dagmc_notwatertight.h5m -o dagmc_watertight.h5m
+make_watertight dagmc_notwatertight.h5m -o dagmc.h5m
 
-build_obb dagmc_watertight.h5m -o dagmc.h5m
-
+# the mbconvert commands are optional but it helps if you want to inspect the geometry
 mbconvert dagmc.h5m dagmc.stl
-
 mbconvert dagmc.h5m dagmc.vtk
 
-python3 example_CAD_simulations.py
+python3 example_CAD_simulation.py
