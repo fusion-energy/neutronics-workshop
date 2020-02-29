48018c783792de5e0d6a0c416c2d25edaca5ea5a
=shimwell <mail@jshimwell.com>
2020-02-29 17:43:26 +0000

    occ_faceter instead of steps2h5m

--- docker_images/build_docker_images.sh
+++ docker_images/build_docker_images.sh
@@ -1,6 +1,6 @@
-docker build -f Dockerfile_openmc -t openmcworkshop/openmc . --no-cache
-docker build -f Dockerfile_openmc_nndc -t openmcworkshop/openmc_nndc . --no-cache
+docker build -f Dockerfile_openmc -t openmcworkshop/openmc .
+docker build -f Dockerfile_openmc_nndc -t openmcworkshop/openmc_nndc .
 
-docker build -f Dockerfile_openmc_dagmc -t openmcworkshop/openmc_dagmc . --no-cache
-docker build -f Dockerfile_openmc_dagmc_nndc -t openmcworkshop/openmc_dagmc_nndc . --no-cache
-docker build -f Dockerfile_workshop -t openmcworkshop/workshop . --no-cache
+docker build -f Dockerfile_openmc_dagmc -t openmcworkshop/openmc_dagmc .
+docker build -f Dockerfile_openmc_dagmc_nndc -t openmcworkshop/openmc_dagmc_nndc .
+docker build -f Dockerfile_workshop -t openmcworkshop/workshop .
