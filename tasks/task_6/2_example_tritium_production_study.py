6305eb29d0f39407e9cf2b81660986f37251452f
shimwell <shimwell@yahoo.com>
2020-03-25 20:13:43 +0000

    simplfied geometry to cut down on simulation time

--- tasks/task_6/2_example_tritium_production_study.py
+++ tasks/task_6/2_example_tritium_production_study.py
@@ -11,41 +11,32 @@ from tqdm import tqdm
 
 def make_materials_geometry_tallies(enrichment):
 
-    # MATERIALS#
+    # MATERIALS
 
-    breeder_material = openmc.Material(name="breeder_material")  # Pb84.2Li15.8
+    breeder_material = openmc.Material(1, "PbLi")  # Pb84.2Li15.8
     breeder_material.add_element('Pb', 84.2, percent_type='ao')
-    breeder_material.add_element('Li', 15.8, percent_type='ao', enrichment=enrichment, enrichment_target='Li6', enrichment_type='ao')  # enrichment defined by function call
+    breeder_material.add_element('Li', 15.8, percent_type='ao', enrichment=enrichment, enrichment_target='Li6', enrichment_type='ao')
     breeder_material.set_density('atom/b-cm', 3.2720171e-2)  # around 11 g/cm3
 
-    copper = openmc.Material(name='copper')
-    copper.set_density('g/cm3', 8.5)
-    copper.add_element('Cu', 1.0)
 
     steel = openmc.Material(name='steel')
     steel.set_density('g/cm3', 7.75)
     steel.add_element('Fe', 0.95, percent_type='wo')
     steel.add_element('C', 0.05, percent_type='wo')
 
-    mats = openmc.Materials([breeder_material, steel, copper])
+    mats = openmc.Materials([breeder_material, steel])
+
 
     # GEOMETRY
 
-    central_sol_surface = openmc.ZCylinder(r=100)
-    central_shield_outer_surface = openmc.ZCylinder(r=110)
+    # surfaces
     vessel_inner = openmc.Sphere(r=500)
     first_wall_outer_surface = openmc.Sphere(r=510)
     breeder_blanket_outer_surface = openmc.Sphere(r=610, boundary_type='vacuum')
 
-    central_sol_region = -central_sol_surface & -vessel_inner
-    central_sol_cell = openmc.Cell(region=central_sol_region)
-    central_sol_cell.fill = copper
 
-    central_shield_region = +central_sol_surface & -central_shield_outer_surface & -vessel_inner
-    central_shield_cell = openmc.Cell(region=central_shield_region)
-    central_shield_cell.fill = steel
-
-    inner_vessel_region = -vessel_inner & + central_shield_outer_surface
+    # cells
+    inner_vessel_region = -vessel_inner
     inner_vessel_cell = openmc.Cell(region=inner_vessel_region)
 
     first_wall_region = -first_wall_outer_surface & +vessel_inner
@@ -55,23 +46,22 @@ def make_materials_geometry_tallies(enrichment):
     breeder_blanket_region = +first_wall_outer_surface & -breeder_blanket_outer_surface
     breeder_blanket_cell = openmc.Cell(region=breeder_blanket_region)
     breeder_blanket_cell.fill = breeder_material
-    breeder_blanket_cell.name = 'breeder_blanket'
 
-    universe = openmc.Universe(cells=[central_sol_cell,central_shield_cell, inner_vessel_cell, first_wall_cell, breeder_blanket_cell])
+    universe = openmc.Universe(cells=[inner_vessel_cell, first_wall_cell, breeder_blanket_cell])
     geom = openmc.Geometry(universe)
 
 
     # SIMULATION SETTINGS
-
-    sett = openmc.Settings()
     batches = 2
+    sett = openmc.Settings()
+    batches = batches
     sett.batches = batches
     sett.inactive = 0
     sett.particles = 5000
     sett.run_mode = 'fixed source'
 
     source = openmc.Source()
-    source.space = openmc.stats.Point((350, 0, 0))
+    source.space = openmc.stats.Point((0, 0, 0))
     source.angle = openmc.stats.Isotropic()
     source.energy = openmc.stats.Discrete([14e6], [1])
     sett.source = source
