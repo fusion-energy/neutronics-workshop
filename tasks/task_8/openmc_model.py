eb38307c8046bb467f1fe13e14c4305d6973fdd7
shimwell <shimwell@yahoo.com>
2020-03-25 22:02:41 +0000

    reduced materials, batches, nps

--- tasks/task_8/openmc_model.py
+++ tasks/task_8/openmc_model.py
@@ -7,82 +7,54 @@ def simulate_model(
     thickness,
     breeder_material_name="Li4SiO4",
     temperature_in_C=500,
-    batches=10,
-    nps=1000,
+    batches=2,
+    nps=5000,
     inner_radius=500,
 ):
 
     # MATERIALS from library of materials in neutronics_material_maker package
-    breeder_material = Material(
-        material_name=breeder_material_name,
-        enrichment=enrichment,
-        temperature_in_C=temperature_in_C,
-    ).neutronics_material
+    breeder_material = Material(material_name=breeder_material_name,
+                                enrichment=enrichment,
+                                temperature_in_C=temperature_in_C,
+                                ).neutronics_material
 
-    eurofer = Material(material_name="eurofer").neutronics_material
+    SS316 = Material(material_name="SS316").neutronics_material
     copper = Material(material_name="copper").neutronics_material
 
-    mats = openmc.Materials([breeder_material, eurofer, copper])
+    mats = openmc.Materials([breeder_material, SS316, copper])
     mats.export_to_xml("materials.xml")
 
     # GEOMETRY#
 
-    central_sol_surface = openmc.ZCylinder(r=100)
-    central_shield_outer_surface = openmc.ZCylinder(r=110)
     first_wall_inner_surface = openmc.Sphere(r=inner_radius)
-    first_wall_outer_surface = openmc.Sphere(r=inner_radius + 10)
-    breeder_blanket_outer_surface = openmc.Sphere(r=inner_radius + 10.0 + thickness)
-    vessel_outer_surface = openmc.Sphere(
-        r=inner_radius + 10.0 + thickness + 10.0, boundary_type="vacuum"
-    )
+    first_wall_outer_surface = openmc.Sphere(r=inner_radius + 10.)
+    breeder_blanket_outer_surface = openmc.Sphere(r=inner_radius + 10. + thickness)
+    vessel_outer_surface = openmc.Sphere(r=inner_radius + 10.0 + thickness + 10.,
+                                         boundary_type="vacuum")
 
-    central_sol_region = -central_sol_surface & -breeder_blanket_outer_surface
-    central_sol_cell = openmc.Cell(region=central_sol_region)
-    central_sol_cell.fill = copper
-
-    central_shield_region = (
-        +central_sol_surface
-        & -central_shield_outer_surface
-        & -breeder_blanket_outer_surface
-    )
-    central_shield_cell = openmc.Cell(region=central_shield_region)
-    central_shield_cell.fill = eurofer
-
-    inner_void_region = -first_wall_inner_surface & +central_shield_outer_surface
+    inner_void_region = -first_wall_inner_surface
     inner_void_cell = openmc.Cell(region=inner_void_region)
     inner_void_cell.name = "inner_void"
 
-    first_wall_region = (
-        -first_wall_outer_surface
-        & +first_wall_inner_surface
-        & +central_shield_outer_surface
-    )
+    first_wall_region = (-first_wall_outer_surface & 
+                        +first_wall_inner_surface)
     first_wall_cell = openmc.Cell(region=first_wall_region)
-    first_wall_cell.fill = eurofer
+    first_wall_cell.fill = SS316
 
-    breeder_blanket_region = (
-        +first_wall_outer_surface
-        & -breeder_blanket_outer_surface
-        & +central_shield_outer_surface
-    )
+    breeder_blanket_region = (+first_wall_outer_surface &
+                             -breeder_blanket_outer_surface)
     breeder_blanket_cell = openmc.Cell(region=breeder_blanket_region)
     breeder_blanket_cell.fill = breeder_material
 
     vessel_region = +breeder_blanket_outer_surface & -vessel_outer_surface
     vessel_cell = openmc.Cell(region=vessel_region)
     vessel_cell.name = "vessel"
-    vessel_cell.fill = eurofer
+    vessel_cell.fill = SS316
 
-    universe = openmc.Universe(
-        cells=[
-            central_sol_cell,
-            central_shield_cell,
-            inner_void_cell,
-            first_wall_cell,
-            breeder_blanket_cell,
-            vessel_cell,
-        ]
-    )
+    universe = openmc.Universe(cells=[inner_void_cell,
+                                      first_wall_cell,
+                                      breeder_blanket_cell,
+                                      vessel_cell])
 
     geom = openmc.Geometry(universe)
 
