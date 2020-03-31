28908555277b4464e89f6adcb6300096da0f8599
shimwell <shimwell@yahoo.com>
2020-03-31 17:36:23 +0100

    1d optimisation working

---
+++ tasks/task_9/get_optimised_values_2d.py
@@ -0,0 +1,27 @@
+
+import json
+
+import adaptive
+from skopt import gp_minimize
+from skopt.utils import dump
+
+from openmc_model import objective
+
+# Optimisation for 2D EXAMPLE
+
+# Uses adaptive sampling methods from task 8 to obtain starting points for the optimiser
+learner = adaptive.Learner2D(objective, bounds=[(0, 100), (10, 200)])
+runner = adaptive.Runner(learner, ntasks=1, goal=lambda l: l.npoints > 30)
+runner.ioloop.run_until_complete(runner.task)
+
+# Gaussian Processes based optimisation that returns an SciPy optimisation object
+res = gp_minimize(objective,          # the function to minimize
+                  dimensions=[(0., 100.), (10., 200.)],       # the bounds on each dimension of x
+                  n_calls=30,         # the number of evaluations of f
+                  n_random_starts=0,  # the number of random initialization points
+                  verbose=True,
+                  x0=[i for i in list(learner.data.keys())], # initial data from the adaptive sampling method
+                  y0=list(learner.data.values()) # initial data from the adaptive sampling method
+                  )
+
+dump(res, 'saved_optimisation_2d.dat')
