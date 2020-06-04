9af9fe4d71b5af16a0ad0181445dd277c47434a6
shimwell <shimwell@yahoo.com>
2020-06-04 18:51:44 +0100

    migrating to breeder to multiplier ratio

--- tasks/task_9/get_true_values_2d.py
+++ tasks/task_9/get_true_values_2d.py
@@ -8,11 +8,12 @@ from openmc_model import objective
 
 # Grid of data values for 2D example
 tbr_values = []
-for enrichment in tqdm(np.linspace(0, 100, 10)):
+for breeder_percent_in_breeder_plus_multiplier in tqdm(np.linspace(0, 100, 10)):
     for thickness in np.linspace(10, 200, 10):
-        tbr_values.append({'enrichment': enrichment,
-                           'thickness': thickness,
-                           'tbr': -objective([enrichment, thickness])
+        tbr_values.append({'breeder_percent_in_breeder_plus_multiplier': breeder_percent_in_breeder_plus_multiplier,
+                           'blanket_breeder_li6_enrichment': blanket_breeder_li6_enrichment,
+                           'tbr': -objective([breeder_percent_in_breeder_plus_multiplier,
+                                              blanket_breeder_li6_enrichment])
                           })
 
 with open('2d_tbr_values.json', mode="w", encoding="utf-8") as f:
