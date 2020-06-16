2cb2e52edc60d9b9e6646a9bb5cceb7b363070cc
shimwell <shimwell@yahoo.com>
2020-06-16 14:08:02 +0100

    added surface filter

--- tasks/task_5/1_example_neutron_spectra.py
+++ tasks/task_5/1_example_neutron_spectra.py
@@ -58,7 +58,8 @@ sett.source = source
 neutron_particle_filter = openmc.ParticleFilter(['neutron'])
 cell_filter = openmc.CellFilter(blanket_cell) # detects particles across a cell / volume
 # surface_filter = openmc.SurfaceFilter(outer_surface) # detects particles across a surface
-energy_filter = openmc.EnergyFilter(openmc.mgxs.GROUP_STRUCTURES['VITAMIN-J-175'])
+energy_bins = openmc.mgxs.GROUP_STRUCTURES['VITAMIN-J-175']
+energy_filter = openmc.EnergyFilter(energy_bins)
 spectra_tally = openmc.Tally(name='blanket_cell_neutron_spectra')
 spectra_tally.filters = [cell_filter, neutron_particle_filter, energy_filter]
 spectra_tally.scores = ['flux']
