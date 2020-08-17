ce82db09bea7e6db2bb5fc5dfa1491efa1b28f93
billingsley-john <johnbillingsley1996@yahoo.com>
2020-08-17 15:46:20 +0100

    added corner points

--- tasks/task_8/1_simulate_with_random_sample.py
+++ tasks/task_8/1_simulate_with_random_sample.py
@@ -18,17 +18,12 @@ from openmc_model import find_tbr_hcpb
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "-n", "--number", default=1, type=int, help="number of simulations to perform"
+    "-n", "--number", default=16, type=int, help="number of simulations to perform"
 )
 args = parser.parse_args()
 
 
-print("running simulations with random sampling")
-
-for i in tqdm(range(args.number)):
-
-    breeder_percent_in_breeder_plus_multiplier_ratio = np.random.uniform(0, 100)
-    blanket_breeder_li6_enrichment = np.random.uniform(1, 100)
+def run_simulation(breeder_percent_in_breeder_plus_multiplier_ratio, blanket_breeder_li6_enrichment):
 
     result = find_tbr_hcpb(breeder_percent_in_breeder_plus_multiplier_ratio,
                            blanket_breeder_li6_enrichment)
@@ -39,3 +34,21 @@ for i in tqdm(range(args.number)):
     Path(filename).parent.mkdir(parents=True, exist_ok=True)
     with open(filename, mode="w", encoding="utf-8") as f:
         json.dump(result, f, indent=4)
+
+
+
+print("running corner simulations")
+
+for i, j in tqdm(zip([0, 0, 100, 100], [0, 100, 0, 100]), total=4):
+
+    run_simulation(i, j)
+
+    
+print("running simulations with random sampling")
+
+for i in tqdm(range(args.number)):
+
+    breeder_percent_in_breeder_plus_multiplier_ratio = np.random.uniform(0, 100)
+    blanket_breeder_li6_enrichment = np.random.uniform(1, 100)
+
+    run_simulation(breeder_percent_in_breeder_plus_multiplier_ratio, blanket_breeder_li6_enrichment)
