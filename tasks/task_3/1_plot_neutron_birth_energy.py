774de6dff32d251ce3cdbc39fdc6531cddd562f3
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2020-11-02 13:45:23 +0000

    updated scripts to work with generic source

--- tasks/task_3/1_plot_neutron_birth_energy.py
+++ tasks/task_3/1_plot_neutron_birth_energy.py
@@ -3,17 +3,33 @@
 """plots different neutron energy distrubutions"""
 
 import openmc
-from source_extraction_utils import make_inital_source, plot_energy_from_initial_source
+from source_extraction_utils import create_inital_particles, plot_energy_from_initial_source
 
+# creates an isotropic point source with monoenergetic 14MeV neutrons
+source = openmc.Source()
+source.space = openmc.stats.Point((0, 0, 0))
+source.angle = openmc.stats.Isotropic()
+source.energy = openmc.stats.Discrete([14e6], [1])
 
-# sets the energy of neutrons to monoenergetic 14MeV source
-make_inital_source(energy=openmc.stats.Discrete([14e6], [1]))
+create_inital_particles(source)
 plot_energy_from_initial_source(input_filename='initial_source.h5')
 
-# sets the energy of neutrons to a fission energy distribution
-make_inital_source(energy=openmc.stats.Watt(a=988000.0, b=2.249e-06))
+
+# creates an isotropic point source with a fission energy distribution
+source = openmc.Source()
+source.space = openmc.stats.Point((0, 0, 0))
+source.angle = openmc.stats.Isotropic()
+source.energy = openmc.stats.Watt(a=988000.0, b=2.249e-06)
+
+create_inital_particles(source)
 plot_energy_from_initial_source(input_filename='initial_source.h5')
 
-# sets the energy of neutrons to a fusion energy distribution, energy is 14.08MeV, atomic mass for D + T = 5, temperature is 20KeV
-make_inital_source(energy=openmc.stats.Muir(e0=14080000.0, m_rat=5.0, kt=20000.0))
+
+# creates an isotropic point source with a fusion energy distribution
+source = openmc.Source()
+source.space = openmc.stats.Point((0, 0, 0))
+source.angle = openmc.stats.Isotropic()
+source.energy = openmc.stats.Muir(e0=14080000.0, m_rat=5.0, kt=20000.0)
+
+create_inital_particles(source)
 plot_energy_from_initial_source(input_filename='initial_source.h5')
