bdf9ea76a24b7a6337e86e2ff3e42e06b15a29b0
Jonathan <mail@jshimwell.com>
2021-08-12 22:38:43 +0100

    pep8 applied using black

--- tasks/task_01_cross_sections/plotting_utils.py
+++ tasks/task_01_cross_sections/plotting_utils.py
@@ -1,4 +1,3 @@
-
 import os
 
 import openmc
@@ -12,11 +11,12 @@ import numpy as np
 
 
 def create_isotope_plot(isotopes, reaction):
-    """Creates a plot of elements and reaction provided
-    """
+    """Creates a plot of elements and reaction provided"""
 
     if reaction not in REACTION_MT.keys():
-        print('Reaction not found, only these reactions are accepted', REACTION_MT.keys())
+        print(
+            "Reaction not found, only these reactions are accepted", REACTION_MT.keys()
+        )
         return None
 
     fig = create_plotly_figure()
@@ -27,19 +27,20 @@ def create_isotope_plot(isotopes, reaction):
         isotope_object = openmc.Nuclide(isotope_name)
 
         energy, cross_sections = openmc.calculate_cexs(
-            isotope_object,
-            'nuclide',
-            [reaction])
+            isotope_object, "nuclide", [reaction]
+        )
         cross_section = cross_sections[0]
         if cross_section.sum() != 0.0:
-            fig.add_trace(go.Scatter(
-                x=energy,
-                y=cross_section,
-                mode='lines',
-                name=isotope_name + ' ' + str(reaction))
+            fig.add_trace(
+                go.Scatter(
+                    x=energy,
+                    y=cross_section,
+                    mode="lines",
+                    name=isotope_name + " " + str(reaction),
+                )
             )
         else:
-            print('isotope ', isotope_name, ' has no cross section data for ', reaction)
+            print("isotope ", isotope_name, " has no cross section data for ", reaction)
 
     return fig
 
@@ -49,17 +50,18 @@ def find_temperature_to_use(list_of_string_temps):
     # 294K is the temperature for endf, others use 293K
     list_of_ints = [int(t[:-1]) for t in list_of_string_temps]
 
-    temp_value = min(list_of_ints, key=lambda x:abs(x-294))
+    temp_value = min(list_of_ints, key=lambda x: abs(x - 294))
 
-    return str(temp_value) + 'K'
+    return str(temp_value) + "K"
 
 
 def create_element_plot(elements, reaction):
-    """Creates a plot of elements and reaction provided
-    """
+    """Creates a plot of elements and reaction provided"""
 
     if reaction not in REACTION_MT.keys():
-        print('Reaction not found, only these reactions are accepted', REACTION_MT.keys())
+        print(
+            "Reaction not found, only these reactions are accepted", REACTION_MT.keys()
+        )
         return None
 
     fig = create_plotly_figure()
@@ -72,23 +74,24 @@ def create_element_plot(elements, reaction):
         try:
             atomic_weight(element_name)
         except ValueError:
-            print('There are no natural isotopes of ', element_name)
+            print("There are no natural isotopes of ", element_name)
             continue
 
         energy, cross_sections = openmc.calculate_cexs(
-            element_object,
-            'element',
-            [reaction])
+            element_object, "element", [reaction]
+        )
         cross_section = cross_sections[0]
         if cross_section.sum() != 0.0:
-            fig.add_trace(go.Scatter(
-                x=energy,
-                y=cross_section,
-                mode='lines',
-                name=element_name + ' ' + str(reaction))
+            fig.add_trace(
+                go.Scatter(
+                    x=energy,
+                    y=cross_section,
+                    mode="lines",
+                    name=element_name + " " + str(reaction),
+                )
             )
         else:
-            print('Element ', element_name, ' has no cross section data for ', reaction)
+            print("Element ", element_name, " has no cross section data for ", reaction)
 
     return fig
 
@@ -96,10 +99,12 @@ def create_element_plot(elements, reaction):
 def create_material_plot(materials, reaction):
 
     if reaction not in REACTION_MT.keys():
-        print('Reaction not found, only these reactions are accepted', REACTION_MT.keys())
+        print(
+            "Reaction not found, only these reactions are accepted", REACTION_MT.keys()
+        )
         return None
 
-    fig = create_plotly_figure(y_axis_label='Macroscopic Cross Section (1/cm)')
+    fig = create_plotly_figure(y_axis_label="Macroscopic Cross Section (1/cm)")
 
     if isinstance(reaction, str):
         REACTION_NUMBER = dict(zip(REACTION_NAME.values(), REACTION_NAME.keys()))
@@ -113,29 +118,29 @@ def create_material_plot(materials, reaction):
 
     for material in materials:
         # extracts energy and cross section for the material for the provided MT reaction mumber
-        energy, xs_data = openmc.calculate_cexs(
-            material,
-            'material',
-            [MT_number])
+        energy, xs_data = openmc.calculate_cexs(material, "material", [MT_number])
 
         # adds the energy dependnat cross sction to the plot
-        fig.add_trace(go.Scatter(
-            x=energy,
-            y=xs_data[0],
-            mode='lines',
-            name=material.name + ' ' + reaction)
+        fig.add_trace(
+            go.Scatter(
+                x=energy,
+                y=xs_data[0],
+                mode="lines",
+                name=material.name + " " + reaction,
+            )
         )
 
     return fig
 
 
 def create_temperature_plot_for_isotope(
-    isotope, temperatures,
-    path_to_wmp='/WMP_Library',
-    reaction='(n,total)',
+    isotope,
+    temperatures,
+    path_to_wmp="/WMP_Library",
+    reaction="(n,total)",
     samples=50000,
     min_energy=1,
-    max_energy=2000
+    max_energy=2000,
 ):
 
     energy = np.linspace(min_energy, max_energy, samples)
@@ -143,72 +148,88 @@ def create_temperature_plot_for_isotope(
     fig = create_plotly_figure()
 
     if reaction not in REACTION_MT.keys():
-        print('Reaction not found, only these reactions are accepted', REACTION_MT.keys())
+        print(
+            "Reaction not found, only these reactions are accepted", REACTION_MT.keys()
+        )
         return None
 
     mt_number = REACTION_MT[reaction]
 
     for temperature in temperatures:
 
-        h5_file = os.path.join(path_to_wmp, nmm.isotope_to_zaid(isotope) + '.h5')
+        h5_file = os.path.join(path_to_wmp, nmm.isotope_to_zaid(isotope) + ".h5")
 
         isotope_multipole = openmc.data.WindowedMultipole.from_hdf5(h5_file)
 
-        fig.add_trace(go.Scatter(
-                        x=energy,
-                        y=isotope_multipole(energy, temperature)[mt_number],
-                        mode='lines',
-                        name=isotope + ' ' + reaction + ' ' + str(temperature)+' Kelvin')
-                    )
+        fig.add_trace(
+            go.Scatter(
+                x=energy,
+                y=isotope_multipole(energy, temperature)[mt_number],
+                mode="lines",
+                name=isotope + " " + reaction + " " + str(temperature) + " Kelvin",
+            )
+        )
     return fig
 
 
-def create_plotly_figure(y_axis_label='Cross section (barns)'):
-    
+def create_plotly_figure(y_axis_label="Cross section (barns)"):
+
     fig = go.Figure()
 
     fig.update_layout(
-          title='Neutron interaction cross sections',
-          xaxis={'title': 'Energy (eV)',
-                 'range': (0, 14.1e6)},
-          yaxis={'title': y_axis_label}
+        title="Neutron interaction cross sections",
+        xaxis={"title": "Energy (eV)", "range": (0, 14.1e6)},
+        yaxis={"title": y_axis_label},
     )
-
 
     # this adds the dropdown box for log and lin axis selection
     fig.update_layout(
         updatemenus=[
             go.layout.Updatemenu(
-                buttons=list([
-                    dict(
-                        args=[{"xaxis.type": 'lin', "yaxis.type": 'lin', 'yaxis.range': (0, 14.1e6)}],
-                        label="linear(x) , linear(y)",
-                        method="relayout"
-                    ),
-                    dict(
-                        args=[{"xaxis.type": 'log', "yaxis.type": 'log'}],
-                        label="log(x) , log(y)",
-                        method="relayout"
-                    ),
-                    dict(
-                        args=[{"xaxis.type": 'log', "yaxis.type": 'lin', 'yaxis.range': (0, 14.1e6)}],
-                        label="log(x) , linear(y)",
-                        method="relayout"
-                    ),
-                    dict(
-                        args=[{"xaxis.type": 'lin', "yaxis.type": 'log'}],
-                        label="linear(x) , log(y)",
-                        method="relayout"
-                    )
-                ]),
+                buttons=list(
+                    [
+                        dict(
+                            args=[
+                                {
+                                    "xaxis.type": "lin",
+                                    "yaxis.type": "lin",
+                                    "yaxis.range": (0, 14.1e6),
+                                }
+                            ],
+                            label="linear(x) , linear(y)",
+                            method="relayout",
+                        ),
+                        dict(
+                            args=[{"xaxis.type": "log", "yaxis.type": "log"}],
+                            label="log(x) , log(y)",
+                            method="relayout",
+                        ),
+                        dict(
+                            args=[
+                                {
+                                    "xaxis.type": "log",
+                                    "yaxis.type": "lin",
+                                    "yaxis.range": (0, 14.1e6),
+                                }
+                            ],
+                            label="log(x) , linear(y)",
+                            method="relayout",
+                        ),
+                        dict(
+                            args=[{"xaxis.type": "lin", "yaxis.type": "log"}],
+                            label="linear(x) , log(y)",
+                            method="relayout",
+                        ),
+                    ]
+                ),
                 pad={"r": 10, "t": 10},
                 showactive=True,
                 x=0.5,
                 xanchor="left",
                 y=1.1,
-                yanchor="top"
+                yanchor="top",
             ),
         ]
     )
-    
+
     return fig
--- tasks/task_08_CSG_mesh_tally/statepoint_to_vtk.py
+++ tasks/task_08_CSG_mesh_tally/statepoint_to_vtk.py
@@ -11,6 +11,7 @@ import math
 import openmc
 import numpy as np
 
+
 def write_moab(xs, ys, zs, tally_label, tally_data, error_data, outfile):
     # attempt to import pymoab
     try:
@@ -19,8 +20,7 @@ def write_moab(xs, ys, zs, tally_label, tally_data, error_data, outfile):
         from pymoab.scd import ScdInterface
         from pymoab import types
     except (ImportError, ModuleNotFoundError):
-        msg = "Conversion to MOAB .h5m file requested," \
-              "but PyMOAB is not installed"
+        msg = "Conversion to MOAB .h5m file requested," "but PyMOAB is not installed"
         raise ImportError(msg)
 
     mb = core.Core()
@@ -31,7 +31,7 @@ def write_moab(xs, ys, zs, tally_label, tally_data, error_data, outfile):
     for k in zs:
         for j in ys:
             for i in xs:
-                coords += [i,j,k]
+                coords += [i, j, k]
 
     low = HomCoord([0, 0, 0, 0])
     high = HomCoord([len(xs) - 1, len(ys) - 1, len(zs) - 1, 0])
@@ -40,22 +40,28 @@ def write_moab(xs, ys, zs, tally_label, tally_data, error_data, outfile):
 
     hexes = mb.get_entities_by_type(0, types.MBHEX)
 
-    tally_tag = mb.tag_get_handle(tally_label,1,types.MB_TYPE_DOUBLE,types.MB_TAG_DENSE,True)
-    error_tag = mb.tag_get_handle("error_tag",1,types.MB_TYPE_DOUBLE,types.MB_TAG_DENSE,True)
+    tally_tag = mb.tag_get_handle(
+        tally_label, 1, types.MB_TYPE_DOUBLE, types.MB_TAG_DENSE, True
+    )
+    error_tag = mb.tag_get_handle(
+        "error_tag", 1, types.MB_TYPE_DOUBLE, types.MB_TAG_DENSE, True
+    )
 
-    mb.tag_set_data(tally_tag,hexes,tally_data)
-    mb.tag_set_data(error_tag,hexes,error_data)
+    mb.tag_set_data(tally_tag, hexes, tally_data)
+    mb.tag_set_data(error_tag, hexes, error_data)
 
-    print('Writing %s' % outfile)
+    print("Writing %s" % outfile)
 
     mb.write_file(outfile)
+
 
 def write_vtk(xs, ys, zs, tally_label, tally_data, error_data, outfile):
     try:
         import vtk
     except (ImportError, ModuleNotFoundError) as e:
-        msg = "Conversion to VTK requested," \
-              "but the Python VTK module is not installed."
+        msg = (
+            "Conversion to VTK requested," "but the Python VTK module is not installed."
+        )
         raise ImportError(msg)
 
     vtk_box = vtk.vtkRectilinearGrid()
@@ -63,18 +69,18 @@ def write_vtk(xs, ys, zs, tally_label, tally_data, error_data, outfile):
     vtk_box.SetDimensions(len(xs), len(ys), len(zs))
 
     vtk_x_array = vtk.vtkDoubleArray()
-    vtk_x_array.SetName('x-coords')
+    vtk_x_array.SetName("x-coords")
     vtk_x_array.SetArray(xs, len(xs), True)
     print(vtk_x_array)
     vtk_box.SetXCoordinates(vtk_x_array)
 
     vtk_y_array = vtk.vtkDoubleArray()
-    vtk_y_array.SetName('y-coords')
+    vtk_y_array.SetName("y-coords")
     vtk_y_array.SetArray(ys, len(ys), True)
     vtk_box.SetYCoordinates(vtk_y_array)
 
     vtk_z_array = vtk.vtkDoubleArray()
-    vtk_z_array.SetName('z-coords')
+    vtk_z_array.SetName("z-coords")
     vtk_z_array.SetArray(zs, len(zs), True)
     vtk_box.SetZCoordinates(vtk_z_array)
 
@@ -97,37 +103,25 @@ def write_vtk(xs, ys, zs, tally_label, tally_data, error_data, outfile):
 
     writer.SetInputData(vtk_box)
 
-    print('Writing %s' % outfile)
+    print("Writing %s" % outfile)
 
     writer.Write()
 
-def initiate_mesh(
-    statepoint_filename,
-    tally_name,
-    output_filename,
-    mesh_id,
-    tally_id
-):
 
-    print('Loading file %s' % statepoint_filename)
+def initiate_mesh(statepoint_filename, tally_name, output_filename, mesh_id, tally_id):
+
+    print("Loading file %s" % statepoint_filename)
 
     sp = openmc.StatePoint(statepoint_filename)
 
-    print('Loading mesh with ID of %s' % mesh_id)
+    print("Loading mesh with ID of %s" % mesh_id)
     mesh = sp.meshes[mesh_id]
 
-    xs = np.linspace(mesh.lower_left[0],
-                     mesh.upper_right[0],
-                     mesh.dimension[0] + 1)
-    ys = np.linspace(mesh.lower_left[1],
-                     mesh.upper_right[1],
-                     mesh.dimension[1] + 1)
-    zs = np.linspace(mesh.lower_left[2],
-                     mesh.upper_right[2],
-                     mesh.dimension[2] + 1)
+    xs = np.linspace(mesh.lower_left[0], mesh.upper_right[0], mesh.dimension[0] + 1)
+    ys = np.linspace(mesh.lower_left[1], mesh.upper_right[1], mesh.dimension[1] + 1)
+    zs = np.linspace(mesh.lower_left[2], mesh.upper_right[2], mesh.dimension[2] + 1)
 
-    tally_args = {'name' : tally_name,
-                  'id'   : tally_id}
+    tally_args = {"name": tally_name, "id": tally_id}
 
     msg = "Loading retrieving tally with \n"
     if tally_name is not None:
@@ -139,19 +133,19 @@ def initiate_mesh(
     except LookupError as e:
         raise e
 
-    data = tally.mean[:,0,0]
-    error = tally.std_dev[:,0,0]
+    data = tally.mean[:, 0, 0]
+    error = tally.std_dev[:, 0, 0]
 
     data = data.tolist()
     error = error.tolist()
 
     for c, i in enumerate(data):
         if math.isnan(i):
-            data[c] = 0.
+            data[c] = 0.0
 
     for c, i in enumerate(error):
         if math.isnan(i):
-            error[c] = 0.
+            error[c] = 0.0
 
     if tally_name is None:
         tally_label = "tally_{}".format(tally_id)
@@ -162,33 +156,31 @@ def initiate_mesh(
         write_vtk(xs, ys, zs, tally_label, data, error, output_filename)
     else:
         write_moab(xs, ys, zs, tally_label, data, error, output_filename)
- 
+
 
 def main():
 
     ap = argparse.ArgumentParser(description=__doc__)
 
-    ap.add_argument('-i','--input',
-                    required=True,
-                    help='Path to statepoint h5 file')
+    ap.add_argument("-i", "--input", required=True, help="Path to statepoint h5 file")
 
-    ap.add_argument('-n','--tally-name',
-                    help='Tally name to add to mesh')
+    ap.add_argument("-n", "--tally-name", help="Tally name to add to mesh")
 
-    ap.add_argument('-t','--tally-id',
-                    type=int,
-                    required=True,
-                    help='Tally name to add to mesh')
+    ap.add_argument(
+        "-t", "--tally-id", type=int, required=True, help="Tally name to add to mesh"
+    )
 
-    ap.add_argument('-m','--mesh-id',
-                    type=int,
-                    required=True,
-                    help='ID of the mesh to convert')
+    ap.add_argument(
+        "-m", "--mesh-id", type=int, required=True, help="ID of the mesh to convert"
+    )
 
-    ap.add_argument('-o', '--output',
-                    action='store',
-                    default='meshtally.vtk',
-                    help='Name of outputfile (.h5m for MOAB, .vtk for VTK)')
+    ap.add_argument(
+        "-o",
+        "--output",
+        action="store",
+        default="meshtally.vtk",
+        help="Name of outputfile (.h5m for MOAB, .vtk for VTK)",
+    )
 
     args = ap.parse_args()
 
@@ -196,8 +188,8 @@ def main():
         statepoint_filename=args.input,
         tally_name=args.tally_name,
         output_filename=args.output,
-        mesh_id = args.mesh_id,
-        tally_id=args.tally_id
+        mesh_id=args.mesh_id,
+        tally_id=args.tally_id,
     )
 
 
