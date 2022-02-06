0684e89036cc4ebae84eaed98751ac31a5b0e146
Jonathan <mail@jshimwell.com>
2022-02-06 22:23:36 +0000

    [skip ci] split example into two sections

--- tasks/task_13_variance_reduction/ww/1_minimal_weight_window_example.py
+++ tasks/task_13_variance_reduction/ww/1_minimal_weight_window_example.py
@@ -78,68 +78,9 @@ fig = plt.subplot()
 # when plotting the 2d data, added the extent is required.
 # otherwise the plot uses the index of the 2d data arrays
 # as the x y axis
-fig.imshow(my_slice_no_ww.mean, extent=[-120,120,-120,120], vmin=1e-5, vmax=10)
+fig.imshow(my_slice_no_ww.mean, extent=[-120,120,-120,120])#, vmin=1e-5, vmax=10)
 plt.savefig('no_ww.png')
-# plt.show()
+plt.show()
 
 
 
-# Create mesh which will be used for tally and weight window
-my_ww_mesh = openmc.RegularMesh()
-my_ww_mesh.dimension = [25, 25, 25]
-my_ww_mesh.lower_left = [-120, -120, -120]  # physical limits (corners) of the mesh
-my_ww_mesh.upper_right = [120, 120, 120]
-
-# imports values for weight windows
-from weight_window_values import upper_ww_bounds, lower_ww_bounds
-
-# docs for ww are here
-# https://docs.openmc.org/en/latest/_modules/openmc/weight_windows.html?highlight=weight%20windows
-ww = openmc.WeightWindows(
-    mesh=my_ww_mesh,
-    upper_ww_bounds=upper_ww_bounds,
-    lower_ww_bounds=lower_ww_bounds,
-    particle_type='neutron',
-    energy_bins=(0.0, 100_000_000.),  # applies this weight window to neutrons of within a large energy range (basically all neutrons in the simulation)
-    survival_ratio=5
-)
-# sets the weight windows define to be used in the simulation
-sett.weight_windows = ww
-
-# deletes old files
-try:
-    os.remove('summary.h5')
-except OSError:
-    pass
-
-# as each particle history is now longer due to the splitting that occurs with
-# weight windows. Running the same number of batches would therefore take more
-# time. To make a fair comparison the batch has been reduce to 20 as this takes
-# a similar amount of time as 100 without weight windows
-sett.batches=20
-
-
-# combines the geometry, materials, settings and tallies to create a neutronics model
-model = openmc.model.Model(geom, mats, sett, tallies)
-
-# runs the simulation with weight windows
-output_ww_filename = model.run()
-
-
-
-
-# open the results file
-results_ww = openmc.StatePoint(output_ww_filename)
-
-# access the flux tally
-my_tally_ww = results_ww.get_tally(scores=['flux'])
-my_slice_ww = my_tally_ww.get_slice(scores=['flux'])
-my_slice_ww.mean.shape = (25, 25)
-fig = plt.subplot()
-
-# when plotting the 2d data, added the extent is required.
-# otherwise the plot uses the index of the 2d data arrays
-# as the x y axis
-fig.imshow(my_slice_ww.mean, extent=[-120,120,-120,120], vmin=1e-5, vmax=10)
-plt.savefig('ww.png')
-# plt.show()
