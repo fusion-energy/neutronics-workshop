86c73c71c3142a9a8d2ad62116caa85969377566
Jonathan Shimwell <mail@jshimwell.com>
2022-12-22 11:47:04 +0000

    added energy grouping for time tally

--- tasks/task_17_detectors_examples/1_time_filter_tally.py
+++ tasks/task_17_detectors_examples/1_time_filter_tally.py
@@ -1,3 +1,12 @@
+# this example has a Helium 3 detector with a point source of neutrons.
+# The emitted neutrons have two energies (14MeV and 2.5MeV)
+# The example shows the neutron flux as a function of time at the detector.
+# The simulation results show the arrival time of the neutrons at the detector
+# The higher energy faster neutrons arrive first and have a smaller spread of
+# arrival times while the lower energy neutrons arrive later and have a larger
+# spread of arrival times across the detector face.
+
+
 import openmc
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,8 +15,8 @@ import matplotlib.pyplot as plt
 # MATERIALS
 
 detector_material = openmc.Material()
-detector_material.add_nuclide('He3', 100., percent_type='ao')
-detector_material.set_density('g/cm3', 0.178e-3)
+detector_material.add_nuclide("He3", 100.0, percent_type="ao")
+detector_material.set_density("g/cm3", 0.178e-3)
 
 materials = openmc.Materials([detector_material])
 
@@ -17,7 +26,7 @@ materials = openmc.Materials([detector_material])
 # surfaces
 
 # large radius as 14MeV neutrons travel at 5.2cm per nano second (1e-9s)
-sphere_surface = openmc.Sphere(r=500, boundary_type='vacuum')
+sphere_surface = openmc.Sphere(r=500, boundary_type="vacuum")
 
 detector_front_surface = openmc.XPlane(x0=100)
 detector_back_surface = openmc.XPlane(x0=110)
@@ -27,12 +36,14 @@ detector_top_surface = openmc.ZPlane(z0=50)
 detector_bottom_surface = openmc.ZPlane(z0=-50)
 
 # cells
-detector_region = +detector_front_surface & \
-                  -detector_back_surface & \
-                  -detector_left_side_surface & \
-                  +detector_right_side_surface & \
-                  -detector_top_surface & \
-                  +detector_bottom_surface
+detector_region = (
+    +detector_front_surface
+    & -detector_back_surface
+    & -detector_left_side_surface
+    & +detector_right_side_surface
+    & -detector_top_surface
+    & +detector_bottom_surface
+)
 
 detector_cell = openmc.Cell(region=detector_region)
 detector_cell.fill = detector_material
@@ -57,7 +68,7 @@ geometry = openmc.Geometry(universe)
 source = openmc.Source()
 source.space = openmc.stats.Point((0, 0, 0))
 source.angle = openmc.stats.Isotropic()
-source.energy = openmc.stats.Discrete([2.5e6, 14e6],[0.5, 0.5])
+source.energy = openmc.stats.Discrete([2.5e6, 14e6], [0.5, 0.5])
 
 
 # SETTINGS
@@ -66,15 +77,15 @@ source.energy = openmc.stats.Discrete([2.5e6, 14e6],[0.5, 0.5])
 settings = openmc.Settings()
 settings.batches = 10
 settings.particles = 50000
-settings.run_mode = 'fixed source'
+settings.run_mode = "fixed source"
 settings.source = source
 
 # TALLIES
 
 tallies = openmc.Tallies()
 
-# 1 nano second to 100 nano second
-time_steps = np.linspace(start=1e-9, stop=100e-9, num= 50)
+# 1 nano second to 100 nano seconds with 500 bins
+time_steps = np.linspace(start=1e-9, stop=100e-9, num=500)
 
 
 time_tally = openmc.Tally(name="time_tally_in_cell")
@@ -93,10 +104,10 @@ sp_filename = model.run()
 sp = openmc.StatePoint(sp_filename)
 
 # access the tally using pandas dataframes
-tally = sp.get_tally(name='time_tally_in_cell')
+tally = sp.get_tally(name="time_tally_in_cell")
 df = tally.get_pandas_dataframe()
 print(df)
 
-# plot the 
-df.plot(x='time low [s]', y='mean')
+# plot the
+df.plot(x="time low [s]", y="mean")
 plt.show()
