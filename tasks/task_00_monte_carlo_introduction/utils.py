d421003580727f9c828f7746a355f890a9b37c4c
Allentro <rossallen1996@gmail.com>
2023-01-06 23:38:52 +0000

    Code improvements from task 0 initial commit

--- tasks/task_00_monte_carlo_introduction/utils.py
+++ tasks/task_00_monte_carlo_introduction/utils.py
@@ -16,24 +16,10 @@ def plot_circle(xy):
         plt.plot(xy[i,0], xy[i,1], f'{color}o')
         plt.xlabel("X-coordinate")
         plt.ylabel("Y-coordinate")
-    return
 
 def calculate_pi(xy): 
     r = np.sqrt(xy[:,0]**2+xy[:,1]**2)
     print(f"For {len(xy):2e} data points, $\pi$ has been estimated as: {round(4*(sum(r<1)/len(xy)),5)}")
-
-def calculate_scatter_angle(current_angle, scatter_angle_mean, scatter_angle_width, distribution='uniform'):
-    if distribution == 'uniform': 
-        scatter_angle = scatter_angle_mean + np.random.uniform(scatter_angle_mean -  scatter_angle_width, scatter_angle_mean + scatter_angle_width)
-    elif distribution == 'gauss': 
-        scatter_angle = scatter_angle_mean + np.random.normal(scatter_angle_mean -  scatter_angle_width, scatter_angle_mean + scatter_angle_width)
-    return scatter_angle+current_angle
-
-def calculate_step(scatter_angle): 
-    x_step = np.cos(scatter_angle)
-    y_step = np.sin(scatter_angle)
-    print(scatter_angle, x_step, y_step)
-    return x_step, y_step
 
 def calculate_scatter_event(current_angle, scatter_angle_mean, scatter_angle_width, mean_free_path, mean_free_path_width, distribution='uniform'): 
     new_angle = calculate_scatter_angle(current_angle, scatter_angle_mean, scatter_angle_width, distribution)
@@ -62,20 +48,13 @@ def calculate_scatter_length(path, width):
         width = np.random.randint(path-width, path+width)
         if width <= 1: 
             return 1
-        else:
-            return width
-
-def calculate_scatter_event(current_angle, scatter_angle_mean, scatter_angle_width, mean_free_path, mean_free_path_width, distribution='uniform'): 
-    new_angle = calculate_scatter_angle(current_angle, scatter_angle_mean, scatter_angle_width, distribution)
-    scatter_length = calculate_scatter_length(mean_free_path, mean_free_path_width)
-    x_step, y_step = calculate_step(new_angle)
-    return x_step, y_step, scatter_length, new_angle
+        return width
 
 def create_particle_track(track_lifetime, scatter_angle_mean, scatter_angle_width, mean_free_path, mean_free_path_width, distribution_type): 
     x = [0] 
     y = [0]
     scatter_length, x_step, y_step, current_angle = mean_free_path, 1, 0, 0
-    for i in range(track_lifetime): 
+    for _ in range(track_lifetime): 
         if scatter_length == 0: 
             x_step, y_step, scatter_length, current_angle = calculate_scatter_event(current_angle, scatter_angle_mean, scatter_angle_width, mean_free_path, mean_free_path_width, distribution=distribution_type)
         x.append(x[-1]+x_step)
@@ -93,4 +72,3 @@ def plot_track(track):
     #plt.legend()
     plt.ylabel("Y-Coordinate")
     plt.xlabel("X-Coorsinate")
-    return
