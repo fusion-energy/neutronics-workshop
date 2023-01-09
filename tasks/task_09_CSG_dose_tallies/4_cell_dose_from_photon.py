f14b3e8e3076fe04d831bb28c985c10d3bd5cc84
Jonathan Shimwell <mail@jshimwell.com>
2023-01-09 17:39:46 +0000

    added unit comments to dose examples

--- tasks/task_09_CSG_dose_tallies/4_cell_dose_from_photon.py
+++ tasks/task_09_CSG_dose_tallies/4_cell_dose_from_photon.py
@@ -14,8 +14,10 @@ import matplotlib.pyplot as plt
 
 # Tissue Equivalent, MS20 from PNNL
 mat_tissue = openmc.Material()
-mat_tissue.add_element("H", 0.546359)
+mat_tissue.add_element("O", 0.079013)
 mat_tissue.add_element("C", 0.32948)
+mat_tissue.add_element("H", 0.546359)
+mat_tissue.add_element("N", 0.008619)
 mat_tissue.add_element("Mg", 0.036358)
 mat_tissue.add_element("Cl", 0.000172)
 mat_tissue.set_density("g/cm3", 1.0)
@@ -29,7 +31,7 @@ mat_air.set_density("g/cm3", 0.001205)
 
 my_materials = openmc.Materials([mat_tissue, mat_air])
 
-yearly_dose = []
+all_dose = []
 distances_to_simulate = [50, 1000, 2000, 4000, 6000]
 for distance_from_source in distances_to_simulate:  # units of cm
 
@@ -69,8 +71,8 @@ for distance_from_source in distances_to_simulate:  # units of cm
     source.space = openmc.stats.Point((0, 0, 0))
     source.angle = openmc.stats.Isotropic()
     # This is a Co60 source, see the task on sources to understand it
-    source.energy = openmc.stats.Discrete([1.1732e6,1.3325e6], [0.5, 0.5])
-    source.particle = 'photon'
+    source.energy = openmc.stats.Discrete([1.1732e6, 1.3325e6], [0.5, 0.5])
+    source.particle = "photon"
 
     my_settings.source = source
 
@@ -80,18 +82,23 @@ for distance_from_source in distances_to_simulate:  # units of cm
 
     # geometry argument refers to irradiation direction
     # https://academic.oup.com/view-large/figure/119655666/ncx112f01.png
-    energy_bins_n, dose_coeffs_n = openmc.data.dose_coefficients(
+    energy_bins_p, dose_coeffs_p = openmc.data.dose_coefficients(
         particle="photon", geometry="AP"
     )
-    energy_function_filter_n = openmc.EnergyFunctionFilter(energy_bins_n, dose_coeffs_n)
-    energy_function_filter_n.interpolation == "cubic"
+    energy_function_filter_p = openmc.EnergyFunctionFilter(energy_bins_p, dose_coeffs_p)
+    energy_function_filter_p.interpolation == "cubic"
 
     photon_particle_filter = openmc.ParticleFilter("photon")
     cell_filter = openmc.CellFilter(phantom_cell)
 
     # Create tally to score dose
     dose_cell_tally = openmc.Tally(name="photon_dose_on_cell")
-    dose_cell_tally.filters = [cell_filter, photon_particle_filter]
+    # note that the EnergyFunctionFilter is included as a filter
+    dose_cell_tally.filters = [
+        cell_filter,
+        photon_particle_filter,
+        energy_function_filter_p,
+    ]
     dose_cell_tally.scores = ["flux"]
     my_tallies = openmc.Tallies([dose_cell_tally])
 
@@ -107,25 +114,24 @@ for distance_from_source in distances_to_simulate:  # units of cm
 
     photons_per_second = 740000000000  # units of photons per second
 
-    # tally.mean is in units of pSv-cm3/source photon
-    # this multiplication changes units to photon to pSv-cm3/second
-    total_dose = photon_tally_result * photons_per_second
+    # converts units from pSv-cm3/source_photon to pSv-cm3/second
+    dose = photon_tally_result * photons_per_second
 
-    # converts from pSv-cm3 to pSv
-    total_dose = total_dose / phantom_volume
+    # converts from pSv-cm3/second to pSv/second
+    dose = dose / phantom_volume
 
-    # converts from pico Sv to micro Sv
-    total_dose = total_dose * 1e-6
+    # converts from (pico) pSv/second to (micro) uSv/second
+    dose = dose * 1e-6
 
-    print(f"dose on phantom is {total_dose}mSv per second")
-    print(f"dose on phantom is {total_dose*60*60*24*365}mSv per year")
+    # converts from uSv/second to uSv/hour
+    dose = dose * 60 * 60
 
-    yearly_dose.append(total_dose * 60 * 60)
+    all_dose.append(dose)
 
-plt.plot(distances_to_simulate, yearly_dose, label="dose on phantom")
+plt.plot(distances_to_simulate, all_dose, label="dose on phantom")
 plt.xlabel("Distance between photon source and phantom")
 plt.ylabel("Dose [uSv per hour]")
-plt.title("Dose on a phantom as a function of distance [cm]\n")
-plt.yscale('log')
+plt.title("Dose on a phantom as a function of distance from a Co60 source\n")
+plt.yscale("log")
 plt.grid(True)
 plt.show()
