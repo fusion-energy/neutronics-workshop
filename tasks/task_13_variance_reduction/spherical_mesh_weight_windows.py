71d6c7f5cd2b2944a89c77dd9eefc82f7bccd946
Jonathan Shimwell <mail@jshimwell.com>
2023-01-24 18:28:41 +0000

    added c++ ww generation

--- tasks/task_13_variance_reduction/spherical_mesh_weight_windows.py
+++ tasks/task_13_variance_reduction/spherical_mesh_weight_windows.py
@@ -6,10 +6,12 @@
 # Another set of weight windows is made from the second flux simulation
 # The weight window value as a function of depth is plotted to show how these improve with each simulation iteration
 
+
+
 import openmc
 import numpy as np
 import matplotlib.pyplot as plt
-
+import openmc.lib
 
 # materials
 mat_water = openmc.Material()
@@ -43,10 +45,11 @@ source.particle = "neutron"
 my_settings = openmc.Settings()
 my_settings.run_mode = "fixed source"
 my_settings.source = source
-my_settings.particles = 10000
-my_settings.batches = 10
+my_settings.particles = 100
+my_settings.batches = 5
 
 # tally
+# mesh = openmc.RegularMesh().from_domain(my_geometry)
 mesh = openmc.SphericalMesh()
 mesh.r_grid = np.linspace(0, outer_surface.r, 100)
 
@@ -62,99 +65,81 @@ my_tallies.append(flux_tally)
 
 # model
 model = openmc.model.Model(my_geometry, my_materials, my_settings, my_tallies)
-output_file = model.run(cwd="no_ww")
 
-import openmc_weight_window_generator
+# analogy running without weight windows
+model.run()
 
-# post process the flux values to create the first batch of weight windows
-with openmc.StatePoint(output_file) as sp:
-    flux_tally = sp.get_tally(id=flux_tally.id)
-    # weight windows from flux
-    weight_windows = sp.generate_wws(tally=flux_tally, rel_err_tol=0.7)[0]
+with openmc.StatePoint(f'statepoint.{my_settings.batches}.h5') as sp:
+    flux_tally = sp.get_tally(id=1)
 
 
-# plot flux against distance
-plt.plot(
-    mesh.r_grid[1:], flux_tally.mean.flatten(), label="flux tally no ww", color="red"
-)
-plt.legend()
-plt.yscale("log")
-plt.xlabel("Radius [cm]")
-plt.ylabel("Flux")
-plt.show()
+model.export_to_xml()
+
+openmc.lib.init()
+
+tally = openmc.lib.tallies[1]
+
+wws = openmc.lib.WeightWindows.from_tally(tally)
+
+openmc.lib.run()
+
+wws.update_weight_windows_magic(tally)
+openmc.lib.settings.weight_windows_on = True
+openmc.lib.run()
+
+with openmc.StatePoint(f'statepoint.{my_settings.batches}.h5') as sp:
+    ww_flux_tally = sp.get_tally(id=1)
+
+# openmc.lib.finalize()
 
 
-# plot weight window against distance
-plt.plot(
-    mesh.r_grid[1:],
-    weight_windows.lower_ww_bounds.flatten(),
-    label="lower ww bounds",
-    color="red",
-)
-plt.plot(
-    mesh.r_grid[1:],
-    weight_windows.upper_ww_bounds.flatten(),
-    label="lower up bounds",
-    color="lightcoral",
-)
-plt.legend()
-plt.xlabel("Radius [cm]")
-plt.ylabel("weight window bound value")
-plt.show()
+# plt.plot(
+#     mesh.r_grid[1:],
+#     ww_flux_tally.mean.flatten(),
+#     label="flux tally ww",
+#     color="red"
+# )
 
-model.settings.weight_windows = weight_windows
-# model update by assigning the weight windows found.
+# plt.plot(
+#     mesh.r_grid[1:],
+#     flux_tally.mean.flatten(),
+#     label="flux tally no ww",
+#     color="blue"
+# )
 
-output_file = model.run(cwd="initial_ww")
-
-with openmc.StatePoint(output_file) as sp:
-    flux_tally_with_ww = sp.get_tally(id=flux_tally.id)
-    # weight windows from flux
-    weight_windows_2 = sp.generate_wws(tally=flux_tally_with_ww, rel_err_tol=0.7)[0]
+# plt.legend()
+# plt.yscale("log")
+# plt.xlabel("Radius [cm]")
+# plt.ylabel("Flux")
+# plt.show()
 
 
-# plot flux with and with out weight windows against distance
-plt.plot(
-    mesh.r_grid[1:], flux_tally.mean.flatten(), label="flux tally no ww", color="red"
-)
-plt.plot(
-    mesh.r_grid[1:],
-    flux_tally_with_ww.mean.flatten(),
-    label="flux tally with ww",
-    color="blue",
-)
-plt.legend()
-plt.yscale("log")
-plt.xlabel("Radius [cm]")
-plt.ylabel("Flux")
-plt.show()
-
-# plot weight window against distance for the firstand second set of weight windows made
-plt.plot(
-    mesh.r_grid[1:],
-    weight_windows.lower_ww_bounds.flatten(),
-    label="lower ww bounds",
-    color="red",
-)
-plt.plot(
-    mesh.r_grid[1:],
-    weight_windows.upper_ww_bounds.flatten(),
-    label="lower up bounds",
-    color="lightcoral",
-)
-plt.plot(
-    mesh.r_grid[1:],
-    weight_windows_2.lower_ww_bounds.flatten(),
-    label="lower ww bounds iteration 2",
-    color="blue",
-)
-plt.plot(
-    mesh.r_grid[1:],
-    weight_windows_2.upper_ww_bounds.flatten(),
-    label="lower up bounds iteration 2",
-    color="cornflowerblue",
-)
-plt.legend()
-plt.xlabel("Radius [cm]")
-plt.ylabel("weight window bound value")
-plt.show()
+    # # plot weight window against distance for the first and second set of weight windows made
+    # plt.plot(
+    #     mesh.r_grid[1:],
+    #     weight_windows.lower_ww_bounds.flatten(),
+    #     label="lower ww bounds",
+    #     color="red",
+    # )
+    # plt.plot(
+    #     mesh.r_grid[1:],
+    #     weight_windows.upper_ww_bounds.flatten(),
+    #     label="lower up bounds",
+    #     color="lightcoral",
+    # )
+    # plt.plot(
+    #     mesh.r_grid[1:],
+    #     weight_windows_2.lower_ww_bounds.flatten(),
+    #     label="lower ww bounds iteration 2",
+    #     color="blue",
+    # )
+    # plt.plot(
+    #     mesh.r_grid[1:],
+    #     weight_windows_2.upper_ww_bounds.flatten(),
+    #     label="lower up bounds iteration 2",
+    #     color="cornflowerblue",
+    # )
+    # plt.legend()
+    # plt.xlabel("Radius [cm]")
+    # plt.ylabel("weight window bound value")
+    # plt.show()
