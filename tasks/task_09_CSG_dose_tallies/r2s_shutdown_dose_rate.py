7718d3042b74749591a4d1d88819b77427586fcb
Jonathan Shimwell <mail@jshimwell.com>
2023-02-01 17:30:50 +0000

    Ni and dense Ni for materials

--- tasks/task_09_CSG_dose_tallies/r2s_shutdown_dose_rate.py
+++ tasks/task_09_CSG_dose_tallies/r2s_shutdown_dose_rate.py
@@ -1,20 +1,17 @@
-# makes a sphere of Silver and irradiates it with a 14MeV neutron source.
+# makes a sphere of nickle and irradiates it with a 2MeV neutron source.
 # The activated material emits gammas and these are transported and tallied
 # on a mesh tally. The resulting decay gamma flux is plotted as a series of
-# images so that the variation over time can be observed. Note that silver
-# activation results in Ag110 which has a half life of 24 seconds. The
-# irradiation and decay timescales are set so that the buildup and decay can be
-# observed
+# images so that the variation / decay over time can be observed.
 from pathlib import Path
 import openmc
 import typing
 
 
-# R2SModel class code is by eepeterson
-# with a few tiny modifications made by shimwell
-# this is not merged into openmc
-# Adding the class to the script to allow usage within the script
-# If this gets merged into openmc then this class can be removed
+# R2SModel class code is by eepeterson.
+# with a few tiny modifications made by Shimwell.
+# this is not yet merged into openmc so extra classes have been included.
+# Adding the class to the script to allow usage within the script.
+# If this gets merged into openmc then this class can be removed.
 class R2SModel(openmc.Model):
     """A Model container for an R2S calculation.
     Parameters
@@ -32,38 +29,45 @@ class R2SModel(openmc.Model):
     photon_settings : Settings object
     photon_tallies : Tallies object
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.ntransport_path = Path('neutron_transport')
-        self.depletion_path = Path('depletion')
-        self.ptransport_path = Path('photon_transport')
+        self.ntransport_path = Path("neutron_transport")
+        self.depletion_path = Path("depletion")
+        self.ptransport_path = Path("photon_transport")
         depletion_options = {}
-        depletion_options['method'] = 'predictor'
-        depletion_options['final_step'] = False
-        operator_kwargs={
-            'normalization_mode': 'source-rate',
-            'dilute_initial': 0,
-            'reduce_chain': True,
-            'reduce_chain_level': 5
+        depletion_options["method"] = "predictor"
+        depletion_options["final_step"] = False
+        operator_kwargs = {
+            "normalization_mode": "source-rate",
+            "dilute_initial": 0,
+            "reduce_chain": True,
+            "reduce_chain_level": 5,
         }
-        depletion_options['operator_kwargs'] = operator_kwargs
+        depletion_options["operator_kwargs"] = operator_kwargs
         self.depletion_options = depletion_options
 
     def execute_run(self, **kwargs):
         """Returns the statepoint filenames from the photon runs"""
         # Do neutron transport and depletion calcs
         self.export_to_xml(self.ntransport_path)
-        #super().run(cwd=self.ntransport_path)
-        self.deplete(self.timesteps,
-                     directory=self.depletion_path,
-                     source_rates=self.source_rates,
-                     **self.depletion_options
-                     )
+        # super().run(cwd=self.ntransport_path)
+        self.deplete(
+            self.timesteps,
+            directory=self.depletion_path,
+            source_rates=self.source_rates,
+            **self.depletion_options,
+        )
 
-        # Read in results and get new depleted materials
-        results = openmc.deplete.Results.from_hdf5(self.depletion_path / "depletion_results.h5")
-        matlist = [results.export_to_materials(i, path=self.depletion_path/'materials.xml')
-                   for i in range(len(self.timesteps))]
+        # Read in simulation results
+        results = openmc.deplete.Results.from_hdf5(
+            self.depletion_path / "depletion_results.h5"
+        )
+        # gets new depleted material objects
+        matlist = [
+            results.export_to_materials(i, path=self.depletion_path / "materials.xml")
+            for i in range(len(self.timesteps))
+        ]
 
         # Set up photon calculation
         self.settings = self.photon_settings
@@ -73,19 +77,21 @@ class R2SModel(openmc.Model):
         statepoint_paths = []
         # Run photon transport for each desired timestep
         for tidx in self.photon_timesteps:
+            # makes a dictionary of materials for easy look up later
             new_mats_by_id = {}
             for new_mat in matlist[tidx]:
                 new_mats_by_id[new_mat.id] = new_mat
-            print('new_mats_by_id', new_mats_by_id)
 
-            rundir = self.ptransport_path / f'timestep_{tidx}'
+            rundir = self.ptransport_path / f"timestep_{tidx}"
 
             # Create Source for every depleted region
             src_list = []
             for cell in self.geometry.get_all_cells().values():
                 if cell.fill is None:
                     continue
-                src = openmc.Source.from_cell_with_material(cell, new_mats_by_id[cell.fill.id])
+                src = openmc.Source.from_cell_with_material(
+                    cell, new_mats_by_id[cell.fill.id]
+                )
                 if src is not None:  # materials may have no photon emission
                     src_list.append(src)
 
@@ -103,10 +109,8 @@ openmc.R2SModel = R2SModel
 class Source(openmc.Source):
     @classmethod
     def from_cell_with_material(
-        cls,
-        cell: openmc.Cell,
-        material: typing.Optional[openmc.Material] = None
-    ) -> typing.Optional['openmc.Source']:
+        cls, cell: openmc.Cell, material: typing.Optional[openmc.Material] = None
+    ) -> typing.Optional["openmc.Source"]:
         """Generate an isotropic photon source from an openmc.Cell object. By
         default the cells material is used to generate the source. If the
         material used to fill the cell has no photon emission then None is
@@ -138,43 +142,41 @@ class Source(openmc.Source):
             )
             raise ValueError(msg)
         if material.volume is None:
-            msg = (
-                "The openmc.Material object used must have the volume "
-                "property set"
-            )
+            msg = "The openmc.Material object used must have the volume " "property set"
             raise ValueError(msg)
 
         photon_spec = material.decay_photon_energy
 
         if photon_spec is None:
-            print('no photon source for this material')
             return None
 
         source = cls(domains=[cell])
-        source.particle = 'photon'
+        source.particle = "photon"
         source.energy = photon_spec
         source.strength = photon_spec.integral()
-        print('source.strength', source.strength)
         source.space = openmc.stats.Box(*cell.bounding_box)
         source.angle = openmc.stats.multivariate.Isotropic()
 
         return source
+
 
 # this patches openmc to include the new R2SModel class
 openmc.Source = Source
 
 # The actual example starts here ...
 
-# First we make a simple geometry
+# First we make a simple geometry with three cells, (two with material)
 sphere_surf_1 = openmc.Sphere(r=20, boundary_type="vacuum")
 sphere_surf_2 = openmc.Sphere(r=5, y0=10)
+sphere_surf_3 = openmc.Sphere(r=5, z0=10)
 
-sphere_region_1 = -sphere_surf_1 & +sphere_surf_2  # void space
+sphere_region_1 = -sphere_surf_1 & +sphere_surf_2 & +sphere_surf_3  # void space
 sphere_region_2 = -sphere_surf_2
+sphere_region_3 = -sphere_surf_3
 
 sphere_cell_1 = openmc.Cell(region=sphere_region_1)
-
 sphere_cell_2 = openmc.Cell(region=sphere_region_2)
+sphere_cell_3 = openmc.Cell(region=sphere_region_3)
 
 # We make a nickle material which should produce a few activation products
 mat_nickle = openmc.Material()
@@ -182,12 +184,23 @@ mat_nickle.id = 1
 mat_nickle.add_element("Ni", 1.0)
 mat_nickle.set_density("g/cm3", 8.9)
 mat_nickle.depletable = True  # must set the depletion to True to deplete the material
-mat_nickle.volume = 523.6  # must set the volume as well as openmc calculates number of atoms
+# must set the volume as well as openmc calculates number of atoms
+mat_nickle.volume = 523.6
 sphere_cell_2.fill = mat_nickle
 
-my_geometry = openmc.Geometry([sphere_cell_1, sphere_cell_2])
+# We make a dense_nickle material which should produce a few more activation products
+mat_dense_nickle = openmc.Material()
+mat_dense_nickle.id = 2
+mat_dense_nickle.add_element("Ni", 1.0)
+mat_dense_nickle.set_density("g/cm3", 19.1)
+mat_dense_nickle.depletable = True  # must set the depletion to True to deplete the material
+# volume must set the volume as well as openmc calculates number of atoms
+mat_dense_nickle.volume = 523.6
+sphere_cell_3.fill = mat_dense_nickle
 
-my_materials = openmc.Materials([mat_nickle])
+my_geometry = openmc.Geometry([sphere_cell_1, sphere_cell_2, sphere_cell_3])
+
+my_materials = openmc.Materials([mat_nickle, mat_dense_nickle])
 
 my_neutron_settings = openmc.Settings()
 my_neutron_settings.batches = 10
@@ -216,10 +229,18 @@ mesh_filter = openmc.MeshFilter(mesh)
 my_photon_tallies = openmc.Tallies()
 # Create mesh filter for tally
 
+# geometry argument refers to irradiation direction
+# https://academic.oup.com/view-large/figure/119655666/ncx112f01.png
+energy_bins_p, dose_coeffs_p = openmc.data.dose_coefficients(
+    particle="photon", geometry="ISO"
+)
+energy_function_filter_p = openmc.EnergyFunctionFilter(energy_bins_p, dose_coeffs_p)
+energy_function_filter_p.interpolation == "cubic"  # as recommended by ICRP
+
 # Create flux mesh tally to score flux
-mesh_tally_1 = openmc.Tally(name="photon_flux_on_mesh")
-particle_filter = openmc.ParticleFilter(['photon'])
-mesh_tally_1.filters = [mesh_filter, particle_filter]
+mesh_tally_1 = openmc.Tally(name="photon_dose_on_mesh")
+particle_filter = openmc.ParticleFilter(["photon"])
+mesh_tally_1.filters = [mesh_filter, particle_filter, energy_function_filter_p]
 mesh_tally_1.scores = ["flux"]
 my_photon_tallies.append(mesh_tally_1)
 
@@ -230,19 +251,20 @@ r2s_model = openmc.R2SModel(
 )
 
 # We define timesteps together with the source rate to make it clearer
+# each timestep is 50 days long
 timesteps_and_source_rates = [
-    (60*60*24*50, 1e20),
-    (60*60*24*50, 0),
-    (60*60*24*50, 0),
-    (60*60*24*50, 0),
-    (60*60*24*50, 0),
-    (60*60*24*50, 0),
-    (60*60*24*50, 0),
-    (60*60*24*50, 0),
-    (60*60*24*50, 0),
-    (60*60*24*50, 0),
-    (60*60*24*50, 0),
-    (60*60*24*50, 0),
+    (60 * 60 * 24 * 50, 1e20),
+    (60 * 60 * 24 * 50, 0),  # cooling timestep with zero neutron flux from here onwards
+    (60 * 60 * 24 * 50, 0),
+    (60 * 60 * 24 * 50, 0),
+    (60 * 60 * 24 * 50, 0),
+    (60 * 60 * 24 * 50, 0),
+    (60 * 60 * 24 * 50, 0),
+    (60 * 60 * 24 * 50, 0),
+    (60 * 60 * 24 * 50, 0),
+    (60 * 60 * 24 * 50, 0),
+    (60 * 60 * 24 * 50, 0),
+    (60 * 60 * 24 * 50, 0),
 ]
 
 # Uses list Python comprehension to get the timesteps and source_rates separately
@@ -253,7 +275,7 @@ r2s_model.timesteps = timesteps
 r2s_model.source_rates = source_rates
 r2s_model.photon_settings = my_photon_settings
 r2s_model.photon_tallies = my_photon_tallies
-# runs photon transport on every timestep but could change to  after [2,3] which would be ever step after the neutron source goes to 0
+# runs photon transport on every timestep
 r2s_model.photon_timesteps = list(range(len(timesteps)))
 
 statepoints = r2s_model.execute_run()
@@ -265,13 +287,12 @@ print([str(s) for s in statepoints])
 # post processing the results and plotting
 
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
 import numpy as np
 import regular_mesh_plotter  # extends openmc.Mesh to include data slice functions
 import openmc_geometry_plot  # extends openmc.Geometry class with plotting functions
 
-my_geometry.view_direction = 'x'
-plotted_part_of_tallys=[]
+my_geometry.view_direction = "x"
+plotted_part_of_tallys = []
 
 material_ids_slice = my_geometry.get_slice_of_material_ids(pixels_across=400)
 
@@ -283,13 +304,25 @@ levels = np.unique([item for sublist in material_ids_slice for item in sublist])
 # subsequent time steps the neutron source is switched off and only decay photons are seen
 for i, statepoint_file in enumerate(statepoints):
     with openmc.StatePoint(statepoint_file) as statepoint:
-        my_tally = statepoint.get_tally(name="photon_flux_on_mesh")
+        my_tally = statepoint.get_tally(name="photon_dose_on_mesh")
         mesh = my_tally.find_filter(openmc.MeshFilter).mesh
-        print(max(my_tally.mean.flatten()))
+
+        neutrons_per_second = 1e18
+
+        # converts units from pSv-cm3/source_neutron to pSv-cm3/second
+        dose = my_tally.mean * neutrons_per_second
+
+        # converts from pSv-cm3/second to pSv/second
+        dose = (
+            dose / mesh.volumes[0][0][0]
+        )  # regular mesh used all voxel have the same volume
+
+        # converts from (pico) pSv/second to (micro) uSv/second
+        dose = dose * 1e-6
 
         tally_slice = mesh.slice_of_data(
-            dataset=my_tally.mean,
-            view_direction='x',
+            dataset=dose,
+            view_direction="x",
             volume_normalization=True,
         )
         fig = plt.figure()
@@ -301,11 +334,12 @@ for i, statepoint_file in enumerate(statepoints):
         )
 
         cbar = plt.colorbar(plot_1)
-        cbar.set_label(f"Photon flux")
+        cbar.set_label(f'Photon dose ["(micro) uSv/second"]')
 
         # adds a contour of the cell geometry
         plt.contour(
-            np.flip(material_ids_slice), # data flipped as mpl operations imshow and contour result in different rotations
+            # data flipped as mpl operations imshow and contour result in different rotations
+            np.rot90(material_ids_slice, 1),
             origin="upper",
             colors="k",
             linestyles="solid",
@@ -314,7 +348,7 @@ for i, statepoint_file in enumerate(statepoints):
             extent=my_geometry.get_mpl_plot_extent(),
         )
 
-        plt.savefig(f'photon_flux_map{str(i).zfill(3)}.png')
+        plt.savefig(f"photon_flux_map{str(i).zfill(3)}.png")
 
         plt.cla()
         plt.clf()
