c0661538983d77d67435fd202961966f2fcc0f5c
Jonathan Shimwell <mail@jshimwell.com>
2023-02-09 16:58:38 +0000

    added photon hand calc

--- tasks/task_09_CSG_dose_tallies/compare_dose_simulation_with_back_of_envelope.py
+++ tasks/task_09_CSG_dose_tallies/compare_dose_simulation_with_back_of_envelope.py
@@ -8,18 +8,29 @@
 import openmc
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def manual_dose_calc(particles_per_shot, distance_from_source):
+def manual_dose_calc(particles_per_shot, distance_from_source, particle, particle_energy):
     # as the model is so simple we can calculate the dose manually by finding
     # neutrons across the surface area of the sphere.
+    # Conversion factor from fluence to dose at 14.1MeV = 495pSv cm2 per neutron (AP)
 
     sphere_surface_area = 4 * math.pi * math.pow(distance_from_source, 2)
 
     particles_per_unit_area = particles_per_shot / sphere_surface_area
 
-    # Conversion factor from fluence to dose at 14.1MeV = 495pSv cm2 per neutron (AP)
-    return particles_per_unit_area * 495e-12
+    # this obtains the does coefficients for the particle
+    energy, dose_coeffs = openmc.data.dose_coefficients(particle, geometry='AP')
+
+    # this gets the index of the particle energy
+    closest_index = np.argmin(np.abs(np.array(energy)-particle_energy))
+
+    dose_coeff = dose_coeffs[closest_index]
+
+    print('dose_coeff', dose_coeff)
+
+    return particles_per_unit_area * dose_coeff * 1e-12
 
 
 
@@ -75,16 +86,16 @@ def simulate_dose(distance_from_source, particle, particles_per_shot, energy):
     energy_function_filter = openmc.EnergyFunctionFilter(energy_bins, dose_coeffs)
     energy_function_filter.interpolation == "cubic"
 
-    neutron_particle_filter = openmc.ParticleFilter("neutron")
+    particle_filter = openmc.ParticleFilter(particle)
     cell_filter = openmc.CellFilter(phantom_cell)
     surface_filter = openmc.SurfaceFilter(phantom_surface)
 
     # Create tally to score dose
-    dose_cell_tally = openmc.Tally(name="neutron_dose_on_cell")
+    dose_cell_tally = openmc.Tally(name="dose_on_cell")
     dose_cell_tally.filters = [
         cell_filter,
         energy_function_filter,
-        neutron_particle_filter
+        particle_filter
     ]
     dose_cell_tally.scores = ["flux"]
 
@@ -100,7 +111,7 @@ def simulate_dose(distance_from_source, particle, particles_per_shot, energy):
     with openmc.StatePoint(statepoint_filename) as statepoint:
 
         tally_result = statepoint.get_tally(
-            name="neutron_dose_on_cell"
+            name="dose_on_cell"
         ).mean.flatten()[0]
 
     # tally.mean is in units of pSv-cm3/source neutron
@@ -116,39 +127,52 @@ def simulate_dose(distance_from_source, particle, particles_per_shot, energy):
     print(f"dose on phantom is {total_dose}Sv per shot")
 
     calculated_dose = manual_dose_calc(
-        particles_per_shot=particles_per_shot, distance_from_source=distance_from_source
+        particles_per_shot=particles_per_shot,
+        distance_from_source=distance_from_source,
+        particle=particle,
+        particle_energy=energy
     )
 
     print(f"dose on phantom is {calculated_dose}Sv per shot")
 
     return total_dose, calculated_dose
 
-particle="neutron"
-energy=14e6
-particles_per_shot=1e18
-dose_for_each_shot_simulation = []
-dose_for_each_shot_calc = []
-distances_to_simulate = [1000, 2000]#, 3000, 4000, 5000, 6000]
-for distance_from_source in distances_to_simulate:  # units of cm
-    total_dose, calculated_dose = simulate_dose(distance_from_source, particle, particles_per_shot, energy)
-    dose_for_each_shot_simulation.append(total_dose)
-    dose_for_each_shot_calc.append(calculated_dose)
 
-plt.plot(
-    distances_to_simulate,
-    dose_for_each_shot_simulation,
-    label="dose on phantom simulation",
-)
-plt.plot(
-    distances_to_simulate, dose_for_each_shot_calc, label="dose on phantom calculation"
-)
-plt.xlabel(f"Distance between {particle} source and phantom [cm]")
-plt.ylabel("Dose [Sv per shot]")
-plt.title(
-    f"Simulation and calculation of dose for different distances between {particle} source and phantom,\n"
-    f"{particles_per_shot} {particle}s per shot "
-)
-plt.legend()
-plt.grid(True, which="both")
-plt.yscale("log")
-plt.savefig(f"{energy}eV_{particle}_dose_as_function_of_distance.png")
+def plot_dose_vs_distance(particle, energy, particles_per_shot):
+    dose_for_each_shot_simulation = []
+    dose_for_each_shot_calc = []
+    distances_to_simulate = [1000, 2000, 3000]#, 4000, 5000, 6000]
+    for distance_from_source in distances_to_simulate:  # units of cm
+        total_dose, calculated_dose = simulate_dose(distance_from_source, particle, particles_per_shot, energy)
+        dose_for_each_shot_simulation.append(total_dose)
+        dose_for_each_shot_calc.append(calculated_dose)
+
+    plt.cla()
+    plt.clf()
+    fig = plt.figure()
+
+    plt.plot(
+        distances_to_simulate,
+        dose_for_each_shot_simulation,
+        label="dose on phantom simulation",
+    )
+    plt.plot(
+        distances_to_simulate,
+        dose_for_each_shot_calc,
+        label="dose on phantom calculation"
+    )
+    plt.xlabel(f"Distance between {particle} source and phantom [cm]")
+    plt.ylabel("Dose [Sv per shot]")
+    plt.title(
+        f"Dose for different distances between {energy: .2e}eV {particle} source and phantom,\n"
+        f"{particles_per_shot: .2e} {particle}s per shot "
+    )
+    plt.legend()
+    plt.grid(True, which="both")
+    plt.yscale("log")
+    plt.tight_layout()
+    plt.savefig(f"{energy}eV_{particle}_dose_as_function_of_distance.png", dpi=400)
+
+
+plot_dose_vs_distance(particle="neutron", energy=14e6, particles_per_shot=1e18)
+plot_dose_vs_distance(particle="photon", energy=16.75e6, particles_per_shot=1e14)
