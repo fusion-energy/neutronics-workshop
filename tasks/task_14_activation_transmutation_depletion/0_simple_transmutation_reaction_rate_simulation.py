a4d321b68c1869e2cec8c9b92f189748f1f9240b
Jonathan Shimwell <mail@jshimwell.com>
2023-07-06 16:05:53 +0100

    corrected z and a usage

--- tasks/task_14_activation_transmutation_depletion/0_simple_transmutation_reaction_rate_simulation.py
+++ tasks/task_14_activation_transmutation_depletion/0_simple_transmutation_reaction_rate_simulation.py
@@ -10,12 +10,12 @@ from openmc.deplete.chain import REACTIONS
 
 # creates a material
 material = openmc.Material()
-material.add_element('Fe', 1)
-material.set_density('g/cm3', 7.7)
+material.add_element("Fe", 1)
+material.set_density("g/cm3", 7.7)
 materials = openmc.Materials([material])
 
 # creates a geometry
-surface = openmc.Sphere(r=500, boundary_type='vacuum')
+surface = openmc.Sphere(r=500, boundary_type="vacuum")
 cell = openmc.Cell(region=-surface)
 cell.fill = material
 geometry = openmc.Geometry([cell])
@@ -25,7 +25,7 @@ settings = openmc.Settings()
 settings.batches = 10
 settings.inactive = 0
 settings.particles = 500
-settings.run_mode = 'fixed source'
+settings.run_mode = "fixed source"
 
 # Create a DT point source
 my_source = openmc.IndependentSource()
@@ -36,7 +36,7 @@ settings.source = my_source
 reactions = list(REACTIONS.keys())
 
 # makes a tally for every nuclide in the material and every reaction
-rr_tally = openmc.Tally(name='RR')
+rr_tally = openmc.Tally(name="RR")
 rr_tally.nuclides = material.get_nuclides()
 rr_tally.scores = reactions
 tallies = openmc.Tallies([rr_tally])
@@ -49,20 +49,20 @@ sp_filename = model.run()
 sp = openmc.StatePoint(sp_filename)
 
 # gets the tally
-tbr_tally = sp.get_tally(name='RR')
+tbr_tally = sp.get_tally(name="RR")
 
 # converts to a pandas dataframe for easy manipulation
 df = tbr_tally.get_pandas_dataframe()
 # sorts the reactions from highest to lowest
-df = df.sort_values('mean', ascending=False)
+df = df.sort_values("mean", ascending=False)
 
 # this loop goes through the reactions getting the reaction rate (mean) for each
 # nuclide (nuclide) and each reaction (score)
-print('Reaction rates per source neutron')
+print("Reaction rates per source neutron")
 for index, row in df.iterrows():
-    if row['mean']!= 0.:
-        score = row['score']
-        nuclide = row['nuclide']
+    if row["mean"] != 0.0:
+        score = row["score"]
+        nuclide = row["nuclide"]
 
         # gets the reaction change in terms of delta atomic number (dA)
         # and delta mass number (dZ). This is a tuple for example (n,2n) is
@@ -71,19 +71,26 @@ for index, row in df.iterrows():
         dAdZ = REACTIONS[score].dadz
 
         # gets the element symbol from the nuclide name for example Fe from Fe56
-        element_symbol = re.split('(\d+)', nuclide)[0]
+        element_symbol = re.split("(\d+)", nuclide)[0]
         # gets the Z number from the nuclide name for example 56 from Fe56
-        Z_number = int(re.split('(\d+)', nuclide)[1])
+        neutron_plus_proton_number = int(re.split("(\d+)", nuclide)[1])
 
         # looks up the atomic number from the element symbol
-        A_number = ATOMIC_NUMBER[element_symbol]
+        proton_number = ATOMIC_NUMBER[element_symbol]
 
         # increases the atomic and mass numbers according to the delta values
-        new_Z = Z_number + dAdZ[1]
-        new_A = A_number + dAdZ[0]
+        new_proton_number = proton_number + dAdZ[1]
+
+        new_neutron_plus_proton_number = neutron_plus_proton_number + dAdZ[0]
+        secondaries = REACTIONS[score].secondaries
+        secondaries_str = ""
+        if len(secondaries) > 0:
+            secondaries_str = f"+{'+'.join(secondaries)}"
 
         # gets the element symbol of the potentially new element
-        new_element_symbol = ATOMIC_SYMBOL[new_A]
+        new_element_symbol = ATOMIC_SYMBOL[new_proton_number]
 
         # prints all the transmutations and the reaction rates
-        print(f"{element_symbol}{Z_number} -> {score} -> {new_element_symbol}{new_Z} per source neutron {row['mean']}")
+        print(
+            f"{element_symbol}{neutron_plus_proton_number} -> {score} -> {new_element_symbol}{new_neutron_plus_proton_number}{secondaries_str} per source neutron {row['mean']}"
+        )
