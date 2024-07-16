76e176db6100b8b5ff70a18f14adfe00ce6216e7
Jonathan Shimwell <mail@jshimwell.com>
2024-07-16 13:01:48 +0200

    added prefix

--- install_scripts/compile_install_ubuntu_22.04.sh
+++ install_scripts/compile_install_ubuntu_22.04.sh
@@ -47,78 +47,136 @@ mamba activate openmc-dagmc
 # install python dependancies
 mamba install -y -c conda-forge numpy cython
 
+
+# install openmc with dagmc embree and double down
+# packages are compiled in this install script using all available CPU cores.
+
+
+sudo apt-get --yes update
+sudo apt-get --yes upgrade
+
+export PREFIX=$HOME/opt/openmc-embree/
+mkdir -p $PREFIX
+
 # installs embree
-cd ~
-git clone --shallow-submodules --single-branch --branch v3.12.2 --depth 1 https://github.com/embree/embree.git
+cd $PREFIX
+mkdir embree
 cd embree
+git clone --shallow-submodules --single-branch --branch v3.12.2 --depth 1 https://github.com/embree/embree.git
 mkdir build
+mkdir install
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=.. -DEMBREE_ISPC_SUPPORT=OFF
-make -j
-make -j install
+cmake $PREFIX/embree/embree -DCMAKE_INSTALL_PREFIX=$PREFIX/embree/install \
+                            -DCMAKE_BUILD_TYPE=Release \
+                            -DEMBREE_ISPC_SUPPORT=OFF \
+                            -DEMBREE_TUTORIALS=OFF \
+                            -DEMBREE_TUTORIALS_GLFW=OFF
+cmake --build . --parallel 32
+cmake --install .
 
 # install moab and pymoab
-cd ~
+cd $PREFIX
 mkdir MOAB
 cd MOAB
 git clone --single-branch -b 5.5.1 --depth 1 https://bitbucket.org/fathomteam/moab/
 mkdir build
+mkdir install 
 cd build
-apt-get install -y libeigen3-dev
-cmake ../moab -DENABLE_PYMOAB=ON -DENABLE_HDF5=ON -DENABLE_BLASLAPACK=OFF -DENABLE_FORTRAN=OFF
-make -j2
-sudo make install
+cmake $PREFIX/MOAB/moab -DCMAKE_INSTALL_PREFIX=$PREFIX/MOAB/install \
+                        -DCMAKE_BUILD_TYPE=Release \
+                        -DENABLE_PYMOAB=ON \
+                        -DENABLE_HDF5=ON \
+                        -DENABLE_BLASLAPACK=OFF \
+                        -DENABLE_FORTRAN=OFF \
+                        -DENABLE_METIS=ON \
+                        -DENABLE_MPI=ON \
+                        -DENABLE_NETCDF=ON \
+                        -DENABLE_PARMETIS=ON \
+                        -DENABLE_PNETCDF=OFF
+
+cmake --build . --parallel 32
+cmake --install .
 
 
 # add to new dirs to the path
-echo 'export PATH="$HOME/MOAB/bin:$PATH"'  >> ~/.bashrc 
-echo 'export LD_LIBRARY_PATH="$HOME/MOAB/lib:$LD_LIBRARY_PATH"'  >> ~/.bashrc 
-source ~/.bashrc 
-mamba activate openmc-dagmc
+echo 'export PREFIX="$HOME/opt/openmc-embree/"' >> $PREFIX/setenv.sh
+echo 'export PATH="$PREFIX/MOAB/install/bin:$PATH"'  >> $PREFIX/setenv.sh
+echo 'export LD_LIBRARY_PATH="$PREFIX/MOAB/install/lib:$LD_LIBRARY_PATH"'  >>  $PREFIX/setenv.sh
+echo 'export PYTHONPATH="$PREFIX/MOAB/install/local/lib/python3.10/dist-packages/:$PYTHONPATH"'  >>  $PREFIX/setenv.sh
+source $PREFIX/setenv.sh
 
 # install Double-Down
-cd ~
-git clone --shallow-submodules --single-branch --branch v1.0.0 --depth 1 https://github.com/pshriwise/double-down.git
+cd $PREFIX
+mkdir double-down
+cd double-down
+git clone --shallow-submodules --single-branch --branch v1.1.0 --depth 1 https://github.com/pshriwise/double-down.git
 cd double-down
 mkdir build
+mkdir install 
 cd build
-cmake .. -DMOAB_DIR=$HOME/MOAB -DCMAKE_INSTALL_PREFIX=.. -DEMBREE_DIR=$HOME/embree
-make -j
-make -j install
-
+cmake $PREFIX/double-down/double-down -DCMAKE_INSTALL_PREFIX=$PREFIX/double-down/install \
+                                      -DCMAKE_BUILD_TYPE=Release \
+                                      -DMOAB_DIR=$PREFIX/MOAB/install \
+                                      -DEMBREE_DIR=$PREFIX/embree/install
+cmake --build . --parallel 32
+cmake --install .
 
 # DAGMC version develop install from source
-cd ~
+cd $PREFIX
 mkdir DAGMC
 cd DAGMC
 git clone --single-branch --branch v3.2.3 --depth 1 https://github.com/svalinn/DAGMC.git
 mkdir build
+mkdir install
 cd build
-cmake ../DAGMC -DBUILD_TALLY=ON -DMOAB_DIR=/usr/local -DDOUBLE_DOWN=ON -DBUILD_STATIC_EXE=OFF -DBUILD_STATIC_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$HOME/DAGMC/ -DDOUBLE_DOWN_DIR=$HOME/double-down
-#cmake ../DAGMC -DBUILD_TALLY=ON -DMOAB_DIR=/usr/local -DCMAKE_INSTALL_PREFIX=$HOME/DAGMC  -DBUILD_STATIC_LIBS=OFF 
-# or build without double down
-# cmake ../DAGMC -DBUILD_TALLY=ON -DMOAB_DIR=/usr/local -DBUILD_STATIC_EXE=OFF -DBUILD_STATIC_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$HOME/DAGMC/
-sudo make -j install
+# it's suspicious that we need to specify libpthread.so...
+cmake $PREFIX/DAGMC/DAGMC -DCMAKE_INSTALL_PREFIX=$PREFIX/DAGMC/install \
+                          -DCMAKE_BUILD_TYPE=Release \
+                          -DBUILD_TALLY=ON \
+                          -DBUILD_TESTS=OFF \
+                          -DBUILD_EXE=OFF \
+                          -DBUILD_BUILD_OBB=OFF \
+                          -DMOAB_DIR=$PREFIX/MOAB/install \
+                          -DDOUBLE_DOWN=ON \
+                          -DDOUBLE_DOWN_DIR=$PREFIX/double-down/install \
+                          -DOpenMP_pthread_LIBRARY=/lib/x86_64-linux-gnu/libpthread.so.0 \
+                          -DBUILD_STATIC_EXE=OFF \
+                          -DBUILD_STATIC_LIBS=OFF
+cmake --build . --parallel 32
+cmake --install .
 
 # add to new dirs to the path
-echo 'export PATH="$HOME/DAGMC/bin:$PATH"'  >> ~/.bashrc 
-echo 'export LD_LIBRARY_PATH="$HOME/DAGMC/lib:$LD_LIBRARY_PATH"'  >> ~/.bashrc 
-source ~/.bashrc 
+echo 'export PATH="$PREFIX/DAGMC/install/bin:$PATH"'  >> $PREFIX/setenv.sh
+echo 'export LD_LIBRARY_PATH="$PREFIX/DAGMC/install/lib:$LD_LIBRARY_PATH"'  >>  $PREFIX/setenv.sh
+source $PREFIX/setenv.sh
 
 # installs OpenMC
-cd ~
-git clone --single-branch --branch develop --depth 1 https://github.com/openmc-dev/openmc.git
+cd $PREFIX
+mkdir openmc
 cd openmc
+git clone --single-branch --branch develop --depth 1 https://github.com/openmc-dev/openmc.git
 mkdir build
+mkdir install
 cd build
-# compile without dagmc
-# cmake ..
-cmake -DOPENMC_USE_DAGMC=ON -DDAGMC_ROOT=$HOME/DAGMC -DHDF5_PREFER_PARALLEL=off .. 
-make -j
-make -j install
-cd ..
-pip install .
+cmake $PREFIX/openmc/openmc -DCMAKE_INSTALL_PREFIX=$PREFIX/openmc/install \
+                            -DCMAKE_BUILD_TYPE=Release \
+                            -DOPENMC_USE_DAGMC=ON \
+                            -DDAGMC_ROOT=$PREFIX/DAGMC/install \
+                            -DOPENMC_USE_MPI=ON \
+                            -DHDF5_PREFER_PARALLEL=ON \
+                            -DCPP20=ON \
+                            -DBUILD_TESTING=OFF \
+                            -DXTENSOR_USE_TBB=OFF \
+                            -DXTENSOR_USE_OPENMP=ON \
+                            -DXTENSOR_USE_XSIMD=OFF
 
+cmake --build . --parallel 32
+cmake --install .
+cd $PREFIX/openmc/openmc
+python -m pip install . --prefix=$PREFIX/openmc/install
+
+echo 'export PATH="$PREFIX/openmc/install/bin:$PATH"'  >> $PREFIX/setenv.sh
+echo 'export PYTHONPATH="$PREFIX/openmc/install/local/lib/python3.10/dist-packages/:$PYTHONPATH"'  >>  $PREFIX/setenv.sh
 # install WMP nuclear data
 RUN wget https://github.com/mit-crpg/WMP_Library/releases/download/v1.1/WMP_Library_v1.1.tar.gz
 tar -xf WMP_Library_v1.1.tar.gz -C
